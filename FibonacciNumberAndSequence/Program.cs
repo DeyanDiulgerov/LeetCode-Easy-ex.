d2c77d5f01d61dac49b2d367f83be20254dad76f
Deyan Diulgerov <chukstv@abv.bg>
2023-05-12 14:32:29 +0300

    Add files via upload

---
+++ FibonacciNumberAndSequence/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumberAndSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\n\n\n-------------------------------------------------------------------------------");
+            Console.WriteLine($"The Fibonacci Number exercise :");
+            Console.WriteLine($"The Fibonacci numbers, commonly denoted F(n) form a sequence," +
+                                $" called the Fibonacci sequence,");
+            Console.WriteLine($"such that each number" +
+                                $" is the sum of the two preceding ones, starting from 0 and 1. That is,,\n");
+            Console.WriteLine($"Given n, calculate F(n).");
+            Console.WriteLine($"-------------------------------------------------------------------------------\n");
+
+            Console.WriteLine("The Fibonacci Sequence exercise :");
+            Console.WriteLine($"Write a program in C# Sharp to find the Fibonacci numbers for a n numbers of series");
+            Console.WriteLine($"Ex. The Fibonnaci Sequence of 10 is:");
+            Console.WriteLine($"0 1 1 2 3 5 8 13 21 34");
+            Console.WriteLine($"-------------------------------------------------------------------------------\n");
+
+            Console.WriteLine($"If you wish you can add another number For Testing");
+            Console.Write($"Your number == ");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nThe Fibonnaci Number of {number} is:\n{FibonacciNumber(number)}");
+            Console.WriteLine();
+            Console.WriteLine($"{FibonacciSequence(number)}]");
+            Console.WriteLine($"-------------------------------------------------------------------------------\n\n");
+
+        }
+
+        public static int FibonacciNumber(int number)
+        {
+            // Fibonacci Series 10
+            //The Fibonacci series of 10 terms is :
+            // 0 1 1 2 3 5 8 13 21 34
+            // 0 + 1 = 1 + 1 = 2 + 1 = 3 + 2 = 5 + 3 = 8 + 5 = 13 + 8 = 21 + 13 = 34 + 21 = 55 + 34 = 89 + 55 = 144
+
+            if (number <= 0)
+                return 0;
+            else
+            {
+                int a = 0, b = 1, c = 1;
+                for (int i = 1; i < number; i++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                return c;
+            }
+        }
+
+        public static int FibonacciSequence(int number)
+        {
+            if (number == 0)
+                return 0;
+            else
+            {
+                int a = 0, b = 1, c = 1;
+                Console.WriteLine($"The Fibonnaci Sequence of {number} is:");
+                Console.Write("[" + a + "," + b + ",");
+                for (int i = 2; i < number; i++)
+                {
+                    c = a + b;
+                    if (i == number - 1)
+                        break;
+                    Console.Write(c + ",");
+                    a = b;
+                    b = c;
+                }
+                return c;
+            }
+        }
+    }
+}
---
+++ TupleOddAndEvenNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleOddAndEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\n------------------------------------------------------");
+            Console.WriteLine($"Write a program to print even or odd numbers in a given range");
+            Console.WriteLine($"------------------------------------------------------\n");
+
+            Console.WriteLine($"Input the range to print starting from 1 : 20:\n");
+            Console.WriteLine(PrintOddAndEven(1, 20));
+            Console.WriteLine($"------------------------------------------------------\n");
+
+            Console.WriteLine($"Input the range to print starting from 3 : 30:\n");
+            Console.WriteLine(PrintOddAndEven(3, 30));
+            Console.WriteLine($"------------------------------------------------------");
+            Console.WriteLine($"------------------------------------------------------\n");
+
+            Console.WriteLine($"If you wish you can add Start number and End number For Testing");
+            Console.Write($"Start number == ");
+            var start = int.Parse(Console.ReadLine());
+            Console.Write($"End number == ");
+            var end = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Input the range to print starting from {start} : {end}:\n");
+            Console.WriteLine(PrintOddAndEven(start, end));
+            Console.WriteLine($"------------------------------------------------------\n\n\n");
+        }
+
+
+        public static Tuple<string, string> PrintOddAndEven(int start, int end)
+        {
+            var evenNum = "";
+            var oddNum = "";
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNum += i + " ";
+                }
+                else
+                {
+                    oddNum += i + " ";
+                }
+            }
+
+            return Tuple.Create($"All even numbers from {start} to {end} are:\n{evenNum}",
+                                $"\n\nAll odd numbers from {start} to {end} are:\n{oddNum}");
+        }
+    }
+}
