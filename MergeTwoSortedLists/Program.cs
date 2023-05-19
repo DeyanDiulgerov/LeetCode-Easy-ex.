c3bedf5bbcd3465e59bd8bac11da4d06f4ce58c9
Deyan Diulgerov <chukstv@abv.bg>
2023-05-19 14:19:54 +0300

    Add files via upload

--- MergeTwoSortedLists/Program.cs
+++ MergeTwoSortedLists/Program.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MergeTwoSortedLists
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            /*You are given the heads of two sorted linked lists list1 and list2.
-            Merge the two lists in a one sorted list.
-            The list should be made by splicing together the nodes of the first two lists.
-            Return the head of the merged linked list.*/
-
         }
     }
 }
