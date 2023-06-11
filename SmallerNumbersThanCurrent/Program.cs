cfd0902881174043ef4b0a758257d94c720877f6
Deyan Diulgerov <chukstv@abv.bg>
2023-06-11 11:53:09 +0300

    Add files via upload

---
+++ SmallerNumbersThanCurrent/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerNumbersThanCurrent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", SmallerNumbersThanCurrent(new int[] { 8, 1, 2, 2, 3 })));
+            Console.WriteLine(String.Join(",", SmallerNumbersThanCurrent(new int[] { 6, 5, 4, 8 })));
+            Console.WriteLine(String.Join(",", SmallerNumbersThanCurrent(new int[] { 7, 7, 7, 7 })));
+        }
+
+
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                counter = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j && nums[j] < nums[i])
+                    {
+                        counter++;
+                    }
+                }
+                result[i] = counter;
+            }
+
+            return result;
+        }
+    }
+}
