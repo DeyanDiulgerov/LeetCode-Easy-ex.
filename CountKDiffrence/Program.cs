ebd4edba0dc790acc5e0d4906e1c541e006c7249
Deyan Diulgerov <chukstv@abv.bg>
2023-06-12 15:07:52 +0300

    Add files via upload

---
+++ CountKDiffrence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountKDiffrence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountKDiffrence(new int[] { 1, 2, 2, 1 }, 1));
+            Console.WriteLine(CountKDiffrence(new int[] { 1, 3 }, 3));
+            Console.WriteLine(CountKDiffrence(new int[] { 3, 2, 1, 5, 4 }, 2));
+        }
+
+        public static int CountKDiffrence(int[] nums, int k)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j && nums[i] - nums[j] == k)
+                        counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
