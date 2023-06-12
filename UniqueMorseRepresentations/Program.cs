b0511555813e20628eecb7eb4bb443eec460c9f6
Deyan Diulgerov <chukstv@abv.bg>
2023-06-12 15:52:18 +0300

    Add files via upload

---
+++ UniqueMorseRepresentations/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueMorseRepresentations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniqueMorseRepresentations(new string[] { "rwjje", "aittjje", "auyyn", "lqtktn", "lmjwn" }));
+            Console.WriteLine(UniqueMorseRepresentations(new string[] { "gin", "zen", "gig", "msg" }));
+            Console.WriteLine(UniqueMorseRepresentations(new string[] { "a" }));
+        }
+
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            if (words.Count() == 1)
+                return 1;
+
+            string[] result = new string[] { };
+            string newResult = "";
+
+            var dictionary = new Dictionary<char, string>()
+            {
+                {'a',".-"},
+                {'b',"-..."},
+                {'c',"-.-."},
+                {'d',"-.."},
+                {'e',"."},
+                {'f',"..-."},
+                {'g',"--."},
+                {'h',"...."},
+                {'i',".."},
+                {'j',".---"},
+                {'k',"-.-"},
+                {'l',".-.."},
+                {'m',"--"},
+                {'n',"-."},
+                {'o',"---"},
+                {'p',".--."},
+                {'q',"--.-"},
+                {'r',".-."},
+                {'s',"..."},
+                {'t',"-"},
+                {'u',"..-"},
+                {'v',"...-"},
+                {'w',".--"},
+                {'x',"-..-"},
+                {'y',"-.--"},
+                {'z',"--.."},
+            };
+
+            var listed = new List<string>(result);
+
+            for (int i = 0; i < words.Count(); i++)
+            {
+                foreach (var letter in words[i])
+                {
+                    newResult += dictionary[letter];
+                }
+                Console.WriteLine(newResult);
+                listed.Add(newResult);
+                newResult = "";
+            }
+
+            return listed.Distinct().Count();
+        }
+    }
+}
