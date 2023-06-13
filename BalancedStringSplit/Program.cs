feae722614deb007df03598a57b139a414ab015b
Deyan Diulgerov <chukstv@abv.bg>
2023-06-13 16:06:57 +0300

    Add files via upload

---
+++ BalancedStringSplit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedStringSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BalancedStringSplit("RLRRLLRLRL"));
+            Console.WriteLine(BalancedStringSplit("RLRRRLLRLL"));
+            Console.WriteLine(BalancedStringSplit("LLLLRRRR"));
+        }
+
+        public static int BalancedStringSplit(string s)
+        {
+            int r = 0, l = 0, count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'R')
+                    r++;
+                else
+                    l++;
+
+                if (r == l)
+                {
+                    count++;
+                    r = 0;
+                    l = 0;
+                }
+            }
+            return count;
+        }
+    }
+}
