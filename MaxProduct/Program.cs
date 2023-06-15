f95ab884b334b524faaa35c40753d7ea995e46f8
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:05:12 +0300

    Update Program.cs

--- MaxProduct/Program.cs
+++ MaxProduct/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace MaxProduct
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(MaxProduct(new int[] { 3, 4, 5, 2 }));
+            Console.WriteLine(MaxProduct(new int[] { 1, 5, 4, 5 }));
+            Console.WriteLine(MaxProduct(new int[] { 3, 7 }));
+        }
+
+        public static int MaxProduct(int[] nums)
+        {
+            var listedNums = new List<int>(nums);
+            listedNums.Sort();
+
+            var highest = listedNums.Last();
+            listedNums.Remove(highest);
+            var secondHighest = listedNums.Last();
+
+            return (highest - 1) * (secondHighest - 1);
         }
     }
 }
