bc857b0fd0f5a13ed7933dda71697762e548b9cb
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:06:27 +0300

    Update Program.cs

--- MissingNumber/Program.cs
+++ MissingNumber/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace MissingNumber
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //Given an array nums containing n distinct numbers in the range [0, n],
+            //return the only number in the range that is missing from the array.
+
+            var array1 = new int[] { 3, 0, 1 };
+            var array2 = new int[] { 0, 1 };
+            var array3 = new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+
+            Console.WriteLine(MissingNumber(array1));
+            Console.WriteLine(MissingNumber(array2));
+            Console.WriteLine(MissingNumber(array3));
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+            // array from 0 to n;
+            var list = new List<int>(nums);
+            list.Sort();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (list[i] != i)
+                {
+                    return i;
+                }
+            }
+            return list.Count();
+
         }
     }
 }
