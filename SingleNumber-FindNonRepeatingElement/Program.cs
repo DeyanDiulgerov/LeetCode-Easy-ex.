557efd57eb7575e358ff7c87abfcbb46ad7acdea
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 12:57:50 +0300

    Update Program.cs

--- SingleNumber-FindNonRepeatingElement/Program.cs
+++ SingleNumber-FindNonRepeatingElement/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace SingleNumber_FindNonRepeatingElement
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(String.Join(",", SingleNumber(new int[] { 2, 2, 1 })));
+            Console.WriteLine(String.Join(",", SingleNumber(new int[] { 4, 1, 2, 1, 2 })));
+            Console.WriteLine(String.Join(",", SingleNumber(new int[] { 1 })));
+            Console.WriteLine(String.Join(",", SingleNumber(new int[] { 1, 1, 2, 2, 3, 4, 5, 4, 5 })));
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            return nums.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).First();
         }
     }
 }
