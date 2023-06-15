7eb0b523f7c12373ca72b4085ea206dde1f1a452
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:07:57 +0300

    Update Program.cs

--- ReversePrefix/Program.cs
+++ ReversePrefix/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace ReversePrefix
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ReversePrefix("abcdefd", 'd'));
+            Console.WriteLine(ReversePrefix("xyxzxe", 'z'));
+            Console.WriteLine(ReversePrefix("abcd", 'z'));
+        }
+
+        public static string ReversePrefix(string word, char ch)
+        {
+            if (!word.Contains(ch))
+                return word;
+
+            var test = "";
+            var reversed = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                test += word[i];
+
+                if (word[i] == ch)
+                    break;
+            }
+
+            for (int i = test.Length - 1; i >= 0; i--)
+                reversed += test[i];
+
+            word = word.Remove(0, test.Length);
+            word = word.Insert(0, reversed);
+
+            return word;
         }
     }
 }
