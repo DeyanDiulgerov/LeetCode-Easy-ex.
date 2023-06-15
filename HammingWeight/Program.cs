25279b97c93d3a76bcc61730f5231d9724f3f5f2
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:03:00 +0300

    Update Program.cs

--- HammingWeight/Program.cs
+++ HammingWeight/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace HammingWeight
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(HammingWeight(00000000000000000000000000001011));
+            Console.WriteLine(HammingWeight(00000000000000000000000010000000));
+            Console.WriteLine(HammingWeight(00000000000000000000000000000100));
+        }
+
+        public static int HammingWeight(uint n)
+        {
+            return Convert.ToString(n, 2).Count(x => x == '1');
         }
     }
 }
