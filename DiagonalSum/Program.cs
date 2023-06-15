efc7f6d5493ee00ed9ed3d533f622f82ff1ea50a
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 15:20:33 +0300

    Update Program.cs

--- DiagonalSum/Program.cs
+++ DiagonalSum/Program.cs
@@ -10,82 +10,59 @@ namespace DiagonalSum
     {
         static void Main(string[] args)
         {
-            int[][] mat1 = new int[][]
+            int[][] matrix1 = new int[][]
             {
-                new int[] {1,2,3},
-                new int[] {4,5,6},
-                new int[] {7,8,9},
+                new int[] {1,2,3 },
+                new int[] {4,5,6 },
+                new int[] {7,8,9 },
             };
-            foreach (var item in mat1)
-            {
-                Console.WriteLine(String.Join(",", item));
-            }
+            Console.WriteLine(DiagonalSumMatrix(matrix1));
 
-            int[][] mat2 = new int[][]
+            int[][] matrix2 = new int[][]
             {
                 new int[] {1,1,1,1 },
-                new int[] {1,1,1,1},
-                new int[] {1,1,1,1},
-                new int[] {1,1,1,1},
+                new int[] {1,1,1,1 },
+                new int[] {1,1,1,1 },
+                new int[] {1,1,1,1 },
             };
-            foreach (var item in mat2)
-            {
-                Console.WriteLine(String.Join(",", item));
-            }
+            Console.WriteLine(DiagonalSumMatrix(matrix2));
 
-            int[][] mat3 = new int[][]
+            int[][] matrix3 = new int[][]
             {
                 new int[] {5 },
             };
-            foreach (var item in mat3)
-            {
-                Console.WriteLine(String.Join(",", item));
-            }
-
-
-            Console.WriteLine(DiagonalSum(mat1));
-            Console.WriteLine(DiagonalSum(mat2));
-            Console.WriteLine(DiagonalSum(mat3));
+            Console.WriteLine(DiagonalSumMatrix(matrix3));
         }
 
-        public static int DiagonalSum(int[][] mat)
+        public static int DiagonalSumMatrix(int[][] mat)
         {
-            if (mat.Length == 1)
-                return mat.First().First();
+            // square matrix
+            int n = mat.GetLength(0);
 
             int sum = 0;
-            var test = new Dictionary<double, double>();
+            var dictNums = new Dictionary<int, int>();
 
-            for (int row = 0; row < mat.Length - 1; row++)
+            for (int row = 0; row <= n; row++)
             {
-                for (int col = 0; col < mat.Length - 1; col++)
+                for (int col = 0; col < n; col++)
                 {
-                    while (row != mat.Length && col != mat.Length)
+                    if (!dictNums.Any(x => x.Key == row && x.Value == col))
                     {
                         sum += mat[row][col];
-                        test.Add(row, col);
-                        row++;
-                        col++;
+                        dictNums.Add(row, col);
                     }
+                    row++;
                 }
             }
 
-            for (int row = mat.Length - 1; row >= 0; row--)
+            for (int row = n - 1; row >= 0; row--)
             {
-                for (int col = 0; col < mat.Length - 1; col++)
+                for (int col = 0; col < n; col++)
                 {
-                    while (true)
-                    {
+                    if (!dictNums.Any(x => x.Key == row && x.Value == col))
+                        sum += mat[row][col];
 
-                        if (!test.Any(x => x.Key == row && x.Value == col))
-                            sum += mat[row][col];
-
-                        if (row == 0 || col == mat.Length)
-                            return sum;
-                        row--;
-                        col++;
-
-                    }
+                    row--;
                 }
             }
 
