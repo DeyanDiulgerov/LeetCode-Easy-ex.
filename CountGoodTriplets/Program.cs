1f2db4e7156e32744e4b9c6e238bc8ea8392f0ef
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 12:58:57 +0300

    Update Program.cs

--- CountGoodTriplets/Program.cs
+++ CountGoodTriplets/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace CountGoodTriplets
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
+            Console.WriteLine(CountGoodTriplets(new int[] { 3, 0, 1, 1, 9, 7 }, 7, 2, 3));
+            Console.WriteLine(CountGoodTriplets(new int[] { 1, 1, 2, 2, 3 }, 0, 0, 1));
+        }
+
+        public static int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+
+                        if (Math.Abs(arr[i] - arr[j]) <= a &&
+                            Math.Abs(arr[j] - arr[k]) <= b &&
+                            Math.Abs(arr[i] - arr[k]) <= c)
+                            counter++;
+                    }
+                }
+            }
+
+            return counter;
         }
     }
 }
