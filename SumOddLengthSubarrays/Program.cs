bce8fa60e5ef511f8d5ed1c38aef11a988aa2b96
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:09:32 +0300

    Update Program.cs

--- SumOddLengthSubarrays/Program.cs
+++ SumOddLengthSubarrays/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace SumOddLengthSubarrays
 {
-    internal class Program
+   class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SumOddLengthSubarrays(new int[] { 1, 4, 2, 5, 3 }));
+            Console.WriteLine(SumOddLengthSubarrays(new int[] { 1, 2 }));
+            Console.WriteLine(SumOddLengthSubarrays(new int[] { 10, 11, 12 }));
+        }
+
+        public static int SumOddLengthSubarrays(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j += 2)
+                {
+                    for (int k = i; k <= j; k++)
+                    {
+                        sum += arr[k];
+                    }
+
+                }
+            }
+            return sum;
         }
     }
 }
