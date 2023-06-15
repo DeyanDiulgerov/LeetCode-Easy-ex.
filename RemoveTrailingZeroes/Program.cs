006a6cb74428ff186e3bfe45a41bd5a9aaecc882
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 13:07:27 +0300

    Update Program.cs

--- RemoveTrailingZeroes/Program.cs
+++ RemoveTrailingZeroes/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace RemoveTrailingZeroes
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(RemoveTrailingZeroes("51230100"));
+            Console.WriteLine(RemoveTrailingZeroes("123"));
+            Console.WriteLine(RemoveTrailingZeroes("1230000000"));
+        }
+
+        public static string RemoveTrailingZeroes(string num)
+        {
+            num = num.TrimEnd('0');
+            return num;
         }
     }
 }
