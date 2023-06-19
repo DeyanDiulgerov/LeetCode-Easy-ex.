ff89b7193e932208c1031e1769c330f00367859d
Deyan Diulgerov <chukstv@abv.bg>
2023-06-19 17:04:27 +0300

    Add files via upload

---
+++ MinOperationsIncreasingArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinOperationsIncreasingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinOperationsIncreasingArray(new int[] { 1, 1, 1 }));
+            Console.WriteLine(MinOperationsIncreasingArray(new int[] { 1, 5, 2, 4, 1 }));
+            Console.WriteLine(MinOperationsIncreasingArray(new int[] { 8 }));
+
+        }
+
+        public static int MinOperationsIncreasingArray(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    while (nums[i] >= nums[j])
+                    {
+                        nums[j]++;
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
