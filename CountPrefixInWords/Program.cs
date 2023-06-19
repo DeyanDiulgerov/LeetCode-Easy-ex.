d28397b4e1f78e401d257cbc39b18b8f6e0bd5f8
Deyan Diulgerov <chukstv@abv.bg>
2023-06-19 17:57:12 +0300

    Add files via upload

---
+++ CountPrefixInWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountPrefixInWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPrefixInWords(new string[]
+            { "pay", "attention", "practice", "attend" }, "at"));
+            Console.WriteLine(CountPrefixInWords(new string[]
+            { "leetcode","win","loops","success" }, "code"));
+        }
+
+        public static int CountPrefixInWords(string[] words, string pref)
+        {
+            int counter = 0;
+
+            foreach (var item in words)
+            {
+                if (item.StartsWith(pref))
+                    counter++;
+            }
+
+            return counter;
+        }
+    }
+}
