aa7572f6360ad485cc7daa2c95a693f1e880639f
Deyan Diulgerov <chukstv@abv.bg>
2023-06-19 09:46:13 +0300

    Add files via upload

---
+++ SeparateDigits/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparateDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", SeparateDigits(new int[] { 13, 25, 83, 77 })));
+            Console.WriteLine(String.Join(",", SeparateDigits(new int[] { 7, 1, 3, 9 })));
+            Console.WriteLine(String.Join(",", SeparateDigits(new int[] { 11, 7, 1, 3, 9 })));
+        }
+
+        public static int[] SeparateDigits(int[] nums)
+        {
+            var result = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= 10)
+                {
+                    var newNum = nums[i].ToString().ToCharArray();
+                    for (int j = 0; j < newNum.Length; j++)
+                    {
+                        result.Add(newNum[j] - 48);
+                    }
+                }
+                else
+                    result.Add(nums[i]);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
