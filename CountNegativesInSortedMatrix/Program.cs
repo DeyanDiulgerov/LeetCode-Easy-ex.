f8dc9b81dee92179e545661edf48ee31a24f8623
Deyan Diulgerov <chukstv@abv.bg>
2023-06-19 17:52:36 +0300

    Add files via upload

---
+++ CountNegativesInSortedMatrix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNegativesInSortedMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int CountNegativesInSortedMatrix(int[][] grid)
+        {
+            int counter = 0;
+
+            foreach (var item in grid)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] < 0)
+                        counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
