0bc9793a1ec2cf6fe68a0eaef4206bd00516e7cc
Deyan Diulgerov <chukstv@abv.bg>
2023-06-20 15:14:08 +0300

    Add files via upload

---
+++ FindNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNumbers(new int[] { 12, 345, 2, 6, 7896 }));
+            Console.WriteLine(FindNumbers(new int[] { 555, 901, 482, 1771 }));
+            Console.WriteLine(FindNumbers(new int[] { 12, 11, 53, 33, 33 }));
+        }
+
+        public static int FindNumbers(int[] nums)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var test = nums[i].ToString().ToCharArray();
+
+                if (test.Length % 2 == 0)
+                    counter++;
+            }
+
+            return counter;
+        }
+    }
+}
