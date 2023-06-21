7c50367f770f80cf18244fe3c1660a367f6d9ee4
Deyan Diulgerov <chukstv@abv.bg>
2023-06-21 14:17:04 +0300

    Add files via upload

---
+++ NumberOfPairs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", NumberOfPairs(new int[] { 1, 2, 3, 4, 5 })));
+            Console.WriteLine(String.Join(",", NumberOfPairs(new int[] { 1, 3, 2, 1, 3, 2, 2 })));
+            Console.WriteLine(String.Join(",", NumberOfPairs(new int[] { 0, 0, 1, 1, 2, 2 })));
+            Console.WriteLine(String.Join(",", NumberOfPairs(new int[] { 1, 1 })));
+            Console.WriteLine(String.Join(",", NumberOfPairs(new int[] { 0 })));
+        }
+
+        public static int[] NumberOfPairs(int[] nums)
+        {
+            int pairCounter = 0;
+            var unique = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (unique.Contains(num))
+                {
+                    pairCounter++;
+                    unique.Remove(num);
+                }
+                else
+                    unique.Add(num);
+            }
+
+            return new int[] { pairCounter, unique.Count() };
+        }
+    }
+}
