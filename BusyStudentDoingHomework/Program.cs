b869b0e3faf587326f232826b12fe4126183ebd6
Deyan Diulgerov <chukstv@abv.bg>
2023-06-21 13:02:59 +0300

    Add files via upload

---
+++ BusyStudentDoingHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusyStudentDoingHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BusyStudentDoingHomework(new int[] { 1, 2, 3 }, new int[] { 3, 2, 7 }, 4));
+            Console.WriteLine(BusyStudentDoingHomework(new int[] { 4 }, new int[] { 4 }, 4));
+            Console.WriteLine(BusyStudentDoingHomework(new int[] { 1, 2, 3 }, new int[] { 3, 2, 7 }, 4));
+        }
+
+        public static int BusyStudentDoingHomework(int[] startTime, int[] endTime, int queryTime)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < startTime.Length; i++)
+            {
+                for (int j = startTime[i]; j <= endTime[i]; j++)
+                {
+                    if (j == queryTime)
+                        counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
