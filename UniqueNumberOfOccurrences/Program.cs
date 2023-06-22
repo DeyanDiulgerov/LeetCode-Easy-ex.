25419a72f383e7395a0d3155a46b3dafb63479a3
Deyan Diulgerov <chukstv@abv.bg>
2023-06-22 12:32:29 +0300

    Add files via upload

---
+++ UniqueNumberOfOccurrences/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueNumberOfOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniqueNumberOfOccurrences(new int[] { 4, 0, 2, -5, -4 }));
+            Console.WriteLine(UniqueNumberOfOccurrences(new int[] { 1, 2, 2, 1, 1, 3 }));
+            Console.WriteLine(UniqueNumberOfOccurrences(new int[] { 1, 2 }));
+            Console.WriteLine(UniqueNumberOfOccurrences(new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 }));
+        }
+
+        public static bool UniqueNumberOfOccurrences(int[] arr)
+        {
+            var numbersAndCount = new Dictionary<int, int>();
+            var counterList = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!numbersAndCount.ContainsKey(arr[i]))
+                    numbersAndCount.Add(arr[i], 1);
+                else
+                    numbersAndCount[arr[i]]++;
+            }
+
+            var testValue = 0;
+            foreach (var kvp in numbersAndCount.OrderBy(x => x.Value))
+            {
+                if (kvp.Value == testValue)
+                    return false;
+
+                testValue = kvp.Value;
+            }
+
+            return true;
+        }
+    }
+}
