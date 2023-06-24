7de02b68adaa1399b984ea628c4d05bad2a8d099
Deyan Diulgerov <chukstv@abv.bg>
2023-06-24 12:12:25 +0300

    Add files via upload

---
+++ MinSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", MinSubsequence(new int[] { 4, 3, 10, 9, 8 })));
+            Console.WriteLine(String.Join(",", MinSubsequence(new int[] { 4, 4, 7, 6, 7 })));
+        }
+
+        public static IList<int> MinSubsequence(int[] nums)
+        {
+            int biggest = 0;
+            var listNums = new List<int>(nums);
+            var retList = new List<int>();
+
+            while (retList.Sum() <= listNums.Sum())
+            {
+                biggest = listNums.Max();
+                retList.Add(biggest);
+                listNums.Remove(biggest);
+            }
+
+            return retList;
+        }
+    }
+}
