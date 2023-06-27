d8f84dba3f14e7b9da1c92bb31a05e4b32d01ce5
Deyan Diulgerov <chukstv@abv.bg>
2023-06-27 13:33:50 +0300

    Add files via upload

---
+++ SortArrayByParityTWO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArrayByParityTWO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", SortArrayByParityTWO(new int[] { 4, 2, 5, 7 })));
+            Console.WriteLine(String.Join(",", SortArrayByParityTWO(new int[] { 2, 3 })));
+        }
+
+        public static int[] SortArrayByParityTWO(int[] nums)
+        {
+            var listedNums = new List<int>(nums);
+            var retList = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = 0;
+
+                if (i % 2 == 0)
+                {
+                    num = listedNums.First(x => x % 2 == 0);
+                    retList.Add(num);
+                    listedNums.Remove(num);
+                }
+                else
+                {
+                    num = listedNums.First(x => x % 2 != 0);
+                    retList.Add(num);
+                    listedNums.Remove(num);
+                }
+            }
+
+            return retList.ToArray();
+        }
+    }
+}
