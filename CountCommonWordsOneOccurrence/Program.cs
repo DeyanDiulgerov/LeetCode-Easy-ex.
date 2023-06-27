1de115fdb8a4ed0a14a9076efc2831fcc0708fc6
Deyan Diulgerov <chukstv@abv.bg>
2023-06-27 18:12:13 +0300

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ CountCommonWordsOneOccurrence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountCommonWordsOneOccurrence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountCommonWordsOneOccurrence(new string[] { "leetcode", "is", "amazing", "as", "is" },
+                new string[] { "amazing", "leetcode", "is" }));
+
+            Console.WriteLine(CountCommonWordsOneOccurrence(new string[] { "b", "bb", "bbb" },
+                new string[] { "a", "aa", "aaa" }));
+
+            Console.WriteLine(CountCommonWordsOneOccurrence(new string[] { "a", "ab" },
+                new string[] { "a", "a", "a", "ab" }));
+        }
+
+        public static int CountCommonWordsOneOccurrence(string[] words1, string[] words2)
+        {
+            var uniqueWords1 = words1.ToLookup(x => x).Where(x => x.Count() == 1).Select(x => x.Key);
+            var uniqueWords2 = words2.ToLookup(x => x).Where(x => x.Count() == 1).Select(x => x.Key);
+
+            return uniqueWords1.Intersect(uniqueWords2).Count();
+        }
+    }
+}
