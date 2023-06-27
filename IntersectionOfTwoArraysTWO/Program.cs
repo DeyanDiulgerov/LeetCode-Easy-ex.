98876ae05beb0c378a0221d136fa25ff21fd57da
Deyan Diulgerov <chukstv@abv.bg>
2023-06-27 13:27:14 +0300

    Add files via upload

---
+++ IntersectionOfTwoArraysTWO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersectionOfTwoArraysTWO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", IntersectionOfTwoArraysTWO(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 })));
+            Console.WriteLine(String.Join(",", IntersectionOfTwoArraysTWO(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 })));
+        }
+
+        public static int[] IntersectionOfTwoArraysTWO(int[] nums1, int[] nums2)
+        {
+            var listedNums = new List<int>();
+            var retList = new List<int>();
+
+            foreach (var item in nums1)
+                listedNums.Add(item);
+
+            foreach (var item in nums2)
+            {
+                if (listedNums.Contains(item))
+                {
+                    retList.Add(item);
+                    listedNums.Remove(item);
+                }
+            }
+
+            return retList.ToArray();
+        }
+    }
+}
