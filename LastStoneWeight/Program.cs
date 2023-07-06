825142e4e49cb9bcbbb82aff6b5d0c03c6986cc6
Deyan Diulgerov <chukstv@abv.bg>
2023-07-06 15:39:28 +0300

    Add files via upload

---
+++ LastStoneWeight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastStoneWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(LastStoneWeight(new int[] { 2, 7, 4, 1, 8, 1 }));
+            Console.WriteLine(LastStoneWeight(new int[] { 1 }));
+        }
+
+        public static int LastStoneWeight(int[] stones)
+        {
+            var listed = new List<int>(stones);
+
+            while (listed.Count() > 1)
+            {
+                int y = listed.Max();
+                listed.Remove(y);
+                int x = listed.Max();
+
+                if (y == x)
+                    listed.Remove(x);
+                else
+                {
+                    listed.Remove(x);
+
+                    var newStone = y - x;
+                    listed.Add(newStone);
+                }
+            }
+
+            if (listed.Count() >= 1)
+                return listed.First();
+            else
+                return 0;
+        }
+    }
+}
