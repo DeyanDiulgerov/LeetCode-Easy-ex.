2f01f9766aec90f51224880aeb48164477cce8d5
Deyan Diulgerov <chukstv@abv.bg>
2023-07-07 13:55:25 +0300

    Add files via upload

---
+++ FindLuckyInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLuckyInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLuckyInteger(new int[] { 2, 2, 3, 4 }));
+            Console.WriteLine(FindLuckyInteger(new int[] { 1, 2, 2, 3, 3, 3 }));
+            Console.WriteLine(FindLuckyInteger(new int[] { 2, 2, 2, 3, 3 }));
+        }
+
+        public static int FindLuckyInteger(int[] arr)
+        {
+            var numAndFrequency = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!numAndFrequency.ContainsKey(arr[i]))
+                    numAndFrequency.Add(arr[i], 1);
+                else
+                    numAndFrequency[arr[i]]++;
+            }
+
+            foreach (var kvp in numAndFrequency.OrderByDescending(x => x.Value))
+            {
+                if (kvp.Key == kvp.Value)
+                    return kvp.Key;
+            }
+
+            return -1;
+        }
+    }
+}
