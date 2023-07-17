843ff957c1b320f0057e63627fc6c46f874ee3c2
Deyan Diulgerov <chukstv@abv.bg>
2023-07-17 17:04:20 +0300

    Add files via upload

---
+++ FindTheHighestAltitude/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheHighestAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheHighestAltitude(new int[] { -5, 1, 5, 0, -7 }));
+            Console.WriteLine(FindTheHighestAltitude(new int[] { -4, -3, -2, -1, 4, 3, 2 }));
+        }
+
+        public static int FindTheHighestAltitude(int[] gain)
+        {
+            int n = gain.Length;
+            int[] result = new int[n + 1];
+
+            result[0] = 0;
+
+            for (int i = 1; i < result.Length; i++)
+            {
+                result[i] = result[i - 1] + gain[i - 1];
+            }
+
+            return result.Max();
+        }
+    }
+}
