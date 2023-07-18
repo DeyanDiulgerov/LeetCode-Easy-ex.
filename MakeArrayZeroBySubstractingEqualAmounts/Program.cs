c3589fe56592cec901c32a37bf6026a7c8d6497d
Deyan Diulgerov <chukstv@abv.bg>
2023-07-18 14:46:16 +0300

    Add files via upload

---
+++ MakeArrayZeroBySubstractingEqualAmounts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeArrayZeroBySubstractingEqualAmounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeArrayZeroBySubstractingEqualAmounts(new int[] { 1, 5, 0, 3, 5 }));
+            Console.WriteLine(MakeArrayZeroBySubstractingEqualAmounts(new int[] { 0 }));
+            Console.WriteLine(MakeArrayZeroBySubstractingEqualAmounts(new int[] { 7, 24, 28, 56 }));
+            Console.WriteLine(MakeArrayZeroBySubstractingEqualAmounts(new int[] { 0, 0, 0, 1 }));
+            Console.WriteLine(MakeArrayZeroBySubstractingEqualAmounts(new int[] { 1, 2, 3, 4 }));
+        }
+
+        public static int MakeArrayZeroBySubstractingEqualAmounts(int[] nums)
+        {
+            return nums.Where(x => x != 0).Distinct().Count();
+        }
+    }
+}
