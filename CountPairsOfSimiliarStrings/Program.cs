626e6b45a6eb6691c0df62453b0a6aea595523fe
Deyan Diulgerov <chukstv@abv.bg>
2023-07-18 16:58:00 +0300

    Add files via upload

---
+++ CountPairsOfSimiliarStrings/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountPairsOfSimiliarStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPairsOfSimiliarStrings(new string[] { "aba", "aabb", "abcd", "bac", "aabc" }));
+            Console.WriteLine(CountPairsOfSimiliarStrings(new string[] { "aabb", "ab", "ba" }));
+            Console.WriteLine(CountPairsOfSimiliarStrings(new string[] { "nba", "cba", "dba" }));
+        }
+
+        public static int CountPairsOfSimiliarStrings(string[] words)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    bool areSimilar = true;
+
+                    foreach (char letter in words[j])
+                    {
+                        if (!words[i].Contains(letter))
+                        {
+                            areSimilar = false;
+                            break;
+                        }
+                    }
+                    foreach (char letter in words[i])
+                    {
+                        if (!words[j].Contains(letter))
+                        {
+                            areSimilar = false;
+                            break;
+                        }
+                    }
+
+                    if (areSimilar)
+                        counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
