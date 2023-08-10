53b17771d6eaa9490b5a0a0549f976547cc4cb38
Deyan Diulgerov <chukstv@abv.bg>
2023-08-10 17:00:02 +0300

    Add files via upload

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void MoveZeroes(int[] nums)
+        {
+            var index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                    continue;
+
+                nums[index] = nums[i];
+
+                if (index != i)
+                    nums[i] = 0;
+
+                index++;
+            }
+
+            return;
+        }
+    }
+}
