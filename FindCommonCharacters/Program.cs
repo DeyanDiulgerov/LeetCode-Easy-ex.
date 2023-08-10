9b8745a2cabe2613e871a5878c99bad1152fb34c
Deyan Diulgerov <chukstv@abv.bg>
2023-08-10 15:40:48 +0300

    Add files via upload

---
+++ FindCommonCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindCommonCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", FindCommonCharacters(
+                new string[] { "cool", "lock", "cook" })));
+            Console.WriteLine(String.Join(",", FindCommonCharacters(
+                new string[] { "bella", "label", "roller" })));
+        }
+
+        public static IList<string> FindCommonCharacters(string[] words)
+        {
+            var result = new List<string>();
+            for (int i = 0; i < words[0].Length; i++)
+            {
+                if (words.All(x => x.Contains(words[0][i])))
+                {
+                    result.Add(words[0][i].ToString());
+                    for (int j = 1; j < words.Length; j++)
+                        words[j] = words[j].Remove(words[j].IndexOf(words[0][i]), 1);
+                }
+            }
+            return result;
+        }
+    }
+}
