8c4899c4176e3c0aa36fd35911d5103778257839
Deyan Diulgerov <chukstv@abv.bg>
2023-08-11 15:46:58 +0300

    Add files via upload

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicates(new int[] { 1, 1, 2 }));
+            Console.WriteLine(RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int i = 1;
+
+            foreach (int n in nums)
+                if (nums[i - 1] != n)
+                    nums[i++] = n;
+
+            return i;
+        }
+    }
+}
