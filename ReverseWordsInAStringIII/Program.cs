1512434090703cd6b433dab2e75e475b6016a91b
Deyan Diulgerov <chukstv@abv.bg>
2023-08-17 16:35:55 +0300

    Add files via upload

---
+++ ReverseWordsInAStringIII/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWordsInAStringIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWordsInAStringIII("Let's take LeetCode contest"));
+            Console.WriteLine(ReverseWordsInAStringIII("God Ding"));
+        }
+
+        public static string ReverseWordsInAStringIII(string s)
+        {
+            var reversed = "";
+            var testWord = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == s.Length - 1)
+                {
+                    testWord += s[i];
+
+                    for (int j = testWord.Length - 1; j >= 0; j--)
+                        reversed += testWord[j];
+                }
+                else if (s[i] == ' ')
+                {
+                    for (int j = testWord.Length - 1; j >= 0; j--)
+                        reversed += testWord[j];
+
+                    reversed += ' ';
+                    testWord = "";
+                }
+                else
+                    testWord += s[i];
+            }
+
+            reversed = reversed.TrimEnd();
+            return reversed;
+        }
+    }
+}
