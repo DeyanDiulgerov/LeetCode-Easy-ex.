fce7042f98acba500d68957ec490d6399b2f380f
Deyan Diulgerov <chukstv@abv.bg>
2023-08-17 15:19:13 +0300

    Add files via upload

---
+++ CheckIfTheSentenceIsPangram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfTheSentenceIsPangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckIfTheSentenceIsPangram("thequickbrownfoxjumpsoverthelazydog"));
+            Console.WriteLine(CheckIfTheSentenceIsPangram("leetcode"));
+        }
+
+        public static bool CheckIfTheSentenceIsPangram(string sentence)
+        {
+            var hashSet = new HashSet<char>();
+
+            for (int i = 0; i < sentence.Length; i++)
+                hashSet.Add(sentence[i]);
+
+            if (hashSet.Count() == 26)
+                return true;
+            else
+                return false;
+        }
+    }
+}
