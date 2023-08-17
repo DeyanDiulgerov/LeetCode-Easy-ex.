19253187cd5d10e85be161e1d456799a2d4a11e6
Deyan Diulgerov <chukstv@abv.bg>
2023-08-17 15:29:00 +0300

    Add files via upload

---
+++ FaultyKeyboard/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultyKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FaultyKeyboard("string"));
+            Console.WriteLine(FaultyKeyboard("poiinter"));
+        }
+
+        public static string FaultyKeyboard(string s)
+        {
+            var result = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'i')
+                {
+                    var reversed = "";
+
+                    for (int j = result.Length - 1; j >= 0; j--)
+                        reversed += result[j];
+
+                    result = reversed;
+                }
+                else
+                    result += s[i];
+            }
+
+            return result;
+        }
+    }
+}
