8a02948b36434fbd6401efaeac72c345d4bd9465
Deyan Diulgerov <chukstv@abv.bg>
2023-08-17 16:43:36 +0300

    Add files via upload

---
+++ CountAsterisks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountAsterisks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountAsterisks("l|*e*et|c**o|*de|"));
+            Console.WriteLine(CountAsterisks("iamprogrammer"));
+            Console.WriteLine(CountAsterisks("yo|uar|e**|b|e***au|tifu|l"));
+        }
+
+        public static int CountAsterisks(string s)
+        {
+            int counter = 0;
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (counter == 2)
+                    counter = 0;
+                if (s[i] == '|')
+                    counter++;
+                if (s[i] == '*' && counter == 0)
+                    result++;
+            }
+            return result;
+        }
+
+    }
+}
