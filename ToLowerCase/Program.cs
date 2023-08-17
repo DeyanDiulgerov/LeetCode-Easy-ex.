5da3312317eb8892227abf97688930ed2939a21d
Deyan Diulgerov <chukstv@abv.bg>
2023-08-17 15:21:58 +0300

    Add files via upload

---
+++ ToLowerCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToLowerCase("Hello"));
+            Console.WriteLine(ToLowerCase("here"));
+            Console.WriteLine(ToLowerCase("LOVELY"));
+        }
+
+        public static string ToLowerCase(string s)
+            => s = s.ToLower();
+    }
+}
