da378d43194cd29b9ae0807278e69e0f5bf53f3a
Deyan Diulgerov <chukstv@abv.bg>
2023-08-18 20:14:20 +0300

    Add files via upload

---
+++ GreatestEnglishLetterInUpperAndLowerCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestEnglishLetterInUpperAndLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GreatestEnglishLetterInUpperAndLowerCase("lEeTcOdE"));
+            Console.WriteLine(GreatestEnglishLetterInUpperAndLowerCase("arRAzFif"));
+            Console.WriteLine(GreatestEnglishLetterInUpperAndLowerCase("AbCdEfGhIjK"));
+        }
+
+        public static string GreatestEnglishLetterInUpperAndLowerCase(string s)
+        {
+            var listedS = new List<string>();
+            foreach (var letter in s)
+                listedS.Add(letter.ToString());
+            listedS.Sort();
+
+            for (int i = listedS.Count() - 1; i >= 0; i--)
+            {
+                if (listedS.Contains(listedS[i].ToString().ToLower())
+                 && listedS.Contains(listedS[i].ToString().ToUpper()))
+                    return listedS[i].ToUpper();
+            }
+
+            return "";
+        }
+    }
+}
