fbb6de950f62cc57174ec12db3049bcbdabbe686
Deyan Diulgerov <chukstv@abv.bg>
2023-08-18 17:38:47 +0300

    Add files via upload

---
+++ FirstLetterToAppearTwice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLetterToAppearTwice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstLetterToAppearTwice("abccbaacz"));
+            Console.WriteLine(FirstLetterToAppearTwice("abcdd"));
+        }
+
+        public static char FirstLetterToAppearTwice(string s)
+        {
+            var allChars = new List<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (allChars.Contains(s[i]))
+                    return s[i];
+
+                allChars.Add(s[i]);
+            }
+
+            return ' ';
+        }
+    }
+}
