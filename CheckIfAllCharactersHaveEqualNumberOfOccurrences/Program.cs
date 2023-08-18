7d5635ef9ced15c2370dbfc73265d6ee1ca76170
Deyan Diulgerov <chukstv@abv.bg>
2023-08-18 16:35:20 +0300

    Add files via upload

---
+++ CheckIfAllCharactersHaveEqualNumberOfOccurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfAllCharactersHaveEqualNumberOfOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckIfAllCharactersHaveEqualNumberOfOccurrences("abacbc"));
+            Console.WriteLine(CheckIfAllCharactersHaveEqualNumberOfOccurrences("aaabb"));
+        }
+
+        public static bool CheckIfAllCharactersHaveEqualNumberOfOccurrences(string s)
+        {
+            var charAndCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!charAndCount.ContainsKey(s[i]))
+                    charAndCount.Add(s[i], 1);
+                else
+                    charAndCount[s[i]]++;
+            }
+
+            if (charAndCount.Values.All(x => x == charAndCount.Values.First()))
+                return true;
+            else
+                return false;
+        }
+    }
+}
