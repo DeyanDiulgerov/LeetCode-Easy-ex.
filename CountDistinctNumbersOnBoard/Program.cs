56b940f580c9a050c9fd09187b8fdd20ed9c87bf
Deyan Diulgerov <chukstv@abv.bg>
2023-08-19 14:20:50 +0300

    Add files via upload

---
+++ CountDistinctNumbersOnBoard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDistinctNumbersOnBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountDistinctNumbersOnBoard(5));
+            Console.WriteLine(CountDistinctNumbersOnBoard(3));
+            Console.WriteLine(CountDistinctNumbersOnBoard(1));
+        }
+
+        public static int CountDistinctNumbersOnBoard(int n)
+        {
+            return n == 1 ? 1 : n - 1;
+        }
+    }
+}
