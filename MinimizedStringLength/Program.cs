9fa851a64df4fce2c87e7da1f0ca194141de8c98
Deyan Diulgerov <chukstv@abv.bg>
2023-08-21 09:08:36 +0300

    Add files via upload

---
+++ MinimizedStringLength/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimizedStringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimizedStringLength("aaabc"));
+            Console.WriteLine(MinimizedStringLength("cbbd"));
+            Console.WriteLine(MinimizedStringLength("dddaaa"));
+        }
+
+        public static int MinimizedStringLength(string s)
+            => s.Distinct().Count();
+    }
+}
