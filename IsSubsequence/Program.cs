d55bda88e7affa264a46623765e289acae3a154d
Deyan Diulgerov <chukstv@abv.bg>
2023-08-24 14:08:31 +0300

    Add files via upload

---
+++ IsSubsequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSubsequence("acb", "ahbgdc"));
+            Console.WriteLine(IsSubsequence("ab", "baab"));
+            Console.WriteLine(IsSubsequence("", ""));
+            Console.WriteLine(IsSubsequence("abc", "ahbgdc"));
+            Console.WriteLine(IsSubsequence("axc", "ahbgdc"));
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s == String.Empty)
+                return true;
+
+            int count = 0;
+
+            for (int i = 0; i < t.Length; i++)
+                if (count < s.Length && s[count] == t[i])
+                    count++;
+
+            return count == s.Length;
+        }
+    }
+}
