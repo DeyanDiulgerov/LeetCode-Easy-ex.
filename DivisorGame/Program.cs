62d21d3351592bd83b6540d0a5d4df98f38babe3
Deyan Diulgerov <chukstv@abv.bg>
2023-08-24 13:51:20 +0300

    Add files via upload

---
+++ DivisorGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivisorGame(2));
+            Console.WriteLine(DivisorGame(3));
+        }
+
+        public static bool DivisorGame(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
