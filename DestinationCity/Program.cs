0f0ef8ffb808838dc386e4a1ecc96853715489d3
Deyan Diulgerov <chukstv@abv.bg>
2023-08-25 11:36:03 +0300

    Add files via upload

---
+++ DestinationCity/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinationCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paths1 = new List<IList<string>>();
+            paths1.Add(new List<string>() { "London", "New York" });
+            paths1.Add(new List<string>() { "New York", "Lima" });
+            paths1.Add(new List<string>() { "Lima", "Sao Paulo" });
+
+            var paths2 = new List<IList<string>>();
+            paths2.Add(new List<string>() { "B", "C" });
+            paths2.Add(new List<string>() { "D", "B" });
+            paths2.Add(new List<string>() { "C", "A" });
+
+            var paths3 = new List<IList<string>>();
+            paths3.Add(new List<string>() { "A", "Z" });
+
+            Console.WriteLine(DestinationCity(paths1));
+            Console.WriteLine(DestinationCity(paths2));
+            Console.WriteLine(DestinationCity(paths3));
+        }
+
+        public static string DestinationCity(IList<IList<string>> paths)
+        {
+            var startCities = new List<string>();
+            var destinationCities = new List<string>();
+
+            foreach (var item in paths)
+            {
+                startCities.Add(item[0]);
+                destinationCities.Add(item[1]);
+            }
+
+            foreach (var item in destinationCities)
+            {
+                if (!startCities.Contains(item))
+                    return item;
+            }
+
+            return "TEST";
+        }
+    }
+}
