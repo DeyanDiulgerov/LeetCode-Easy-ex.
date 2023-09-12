34a15c6fa830e822afa83cc73fb6d6b2dc9aa38d
Deyan Diulgerov <chukstv@abv.bg>
2023-09-12 15:28:22 +0300

    Add files via upload

---
+++ CountElementsWithSmallerAndGreaterElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountElementsWithSmallerAndGreaterElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountElementsWithSmallerAndGreaterElements(new int[] { 11, 7, 2, 15 }));
+            Console.WriteLine(CountElementsWithSmallerAndGreaterElements(new int[] { -3, 3, 3, 90 }));
+        }
+
+        public static int CountElementsWithSmallerAndGreaterElements(int[] nums)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums.Any(x => x > nums[i]) && nums.Any(x => x < nums[i]))
+                    counter++;
+            }
+
+            return counter;
+        }
+    }
+}
