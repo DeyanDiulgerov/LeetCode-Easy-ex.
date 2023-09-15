fabd6c3fd5038b6d06e7a632314b825f73fe1dfb
Deyan Diulgerov <chukstv@abv.bg>
2023-09-15 17:18:42 +0300

    Add files via upload

---
+++ ElementAppearingMoreThan25Percent/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementAppearingMoreThan25Percent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ElementAppearingMoreThan25Percent(new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 }));
+            Console.WriteLine(ElementAppearingMoreThan25Percent(new int[] { 1, 1 }));
+        }
+
+        public static int ElementAppearingMoreThan25Percent(int[] arr)
+        {
+            var numAndAppeareance = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!numAndAppeareance.ContainsKey(arr[i]))
+                    numAndAppeareance.Add(arr[i], 1);
+                else
+                    numAndAppeareance[arr[i]]++;
+            }
+
+            foreach (var kvp in numAndAppeareance.OrderByDescending(x => x.Value))
+            {
+                return kvp.Key;
+            }
+
+            return -1;
+        }
+    }
+}
