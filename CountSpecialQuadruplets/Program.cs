6a2e4e58166e595d0375d3a84aa9a54b22f7be39
Deyan Diulgerov <chukstv@abv.bg>
2023-09-16 15:04:34 +0300

    Add files via upload

---
+++ CountSpecialQuadruplets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpecialQuadruplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSpecialQuadruplets(new int[] { 1, 2, 3, 6 }));
+            Console.WriteLine(CountSpecialQuadruplets(new int[] { 3, 3, 6, 4, 5 }));
+            Console.WriteLine(CountSpecialQuadruplets(new int[] { 1, 1, 1, 3, 5 }));
+        }
+
+        public static int CountSpecialQuadruplets(int[] nums)
+        {
+            int counter = 0;
+
+            for (int a = 0; a < nums.Length - 3; a++)
+            {
+                for (int b = a + 1; b < nums.Length - 2; b++)
+                {
+                    for (int c = b + 1; c < nums.Length - 1; c++)
+                    {
+                        for (int d = c + 1; d < nums.Length; d++)
+                        {
+                            if (nums[a] + nums[b] + nums[c] == nums[d])
+                                counter++;
+                        }
+                    }
+                }
+            }
+            return counter;
+        }
+    }
+}
