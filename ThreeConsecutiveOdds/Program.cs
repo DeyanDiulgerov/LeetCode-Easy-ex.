6391b0ce045a4330e877cfd5ab1a25794e010554
Deyan Diulgerov <chukstv@abv.bg>
2023-09-16 14:46:48 +0300

    Update Program.cs

--- ThreeConsecutiveOdds/Program.cs
+++ ThreeConsecutiveOdds/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace ThreeConsecutiveOdds
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ThreeConsecutiveOdds(new int[] { 1, 2, 1, 1 }));
+            Console.WriteLine(ThreeConsecutiveOdds(new int[] { 1, 2, 34, 3, 4, 5, 7, 23, 12 }));
+            Console.WriteLine(ThreeConsecutiveOdds(new int[] { 2, 6, 4, 1 }));
+        }
+
+        public static bool ThreeConsecutiveOdds(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                var testList = new List<int>();
+
+                for (int j = i; j < i + 3; j++)
+                {
+                    if (arr[j] % 2 != 0)
+                        testList.Add(arr[j]);
+                }
+
+                if (testList.Count() == 3)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
