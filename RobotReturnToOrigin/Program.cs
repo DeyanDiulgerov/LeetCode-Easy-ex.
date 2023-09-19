c371910aa32e1785c2d3eaae58870b846f85074b
Deyan Diulgerov <chukstv@abv.bg>
2023-09-19 14:37:51 +0300

    Add files via upload

---
+++ RobotReturnToOrigin/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotReturnToOrigin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RobotReturnToOrigin("UD"));
+            Console.WriteLine(RobotReturnToOrigin("LL"));
+        }
+
+        public static bool RobotReturnToOrigin(string moves)
+        {
+            var moveAndCountDict = new Dictionary<char, int>();
+
+            moveAndCountDict.Add('L', 0);
+            moveAndCountDict.Add('R', 0);
+            moveAndCountDict.Add('U', 0);
+            moveAndCountDict.Add('D', 0);
+
+            for (int i = 0; i < moves.Length; i++)
+                moveAndCountDict[moves[i]]++;
+
+            if (moveAndCountDict['L'] == moveAndCountDict['R']
+             && moveAndCountDict['D'] == moveAndCountDict['U'])
+                return true;
+            else
+                return false;
+        }
+    }
+}
