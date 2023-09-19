262b8057c4aea28c730d907945ac72d716f51df8
Deyan Diulgerov <chukstv@abv.bg>
2023-09-19 14:45:23 +0300

    Add files via upload

---
+++ PercentageOfLetterInString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentageOfLetterInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PercentageOfLetterInString("foobar", 'o'));
+            Console.WriteLine(PercentageOfLetterInString("jjjj", 'k'));
+            Console.WriteLine(PercentageOfLetterInString("sgawtb", 's'));
+        }
+
+        public static int PercentageOfLetterInString(string s, char letter)
+        {
+            if (!s.Contains(letter))
+                return 0;
+
+            double count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == letter)
+                    count++;
+            }
+
+            return (int)Math.Floor((count / s.Length) * 100);
+        }
+    }
+}
