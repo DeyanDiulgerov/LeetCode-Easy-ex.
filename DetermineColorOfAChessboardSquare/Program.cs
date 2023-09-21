0036f5aee3d6ef06ed5115222f2757182b6341aa
Deyan Diulgerov <chukstv@abv.bg>
2023-09-22 01:42:37 +0300

    Add files via upload

---
+++ DetermineColorOfAChessboardSquare/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetermineColorOfAChessboardSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DetermineColorOfAChessboardSquare("a1"));
+            Console.WriteLine(DetermineColorOfAChessboardSquare("h3"));
+            Console.WriteLine(DetermineColorOfAChessboardSquare("c7"));
+        }
+
+        public static bool DetermineColorOfAChessboardSquare(string coordinates)
+        {
+            int letter = coordinates[0] - 'a';
+            int digit = coordinates[1] - '0';
+            return (letter + digit) % 2 == 0;
+        }
+    }
+}
