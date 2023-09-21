0fddc48720b071b0d04b61f7fcd69ac1918fe51d
Deyan Diulgerov <chukstv@abv.bg>
2023-09-21 15:46:37 +0300

    Add files via upload

---
+++ MakeTheStringGreat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeTheStringGreat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeTheStringGreat("kkdsFuqUfSDKK"));
+            Console.WriteLine(MakeTheStringGreat("jeSsEJ"));
+            Console.WriteLine(MakeTheStringGreat("Pp"));
+            Console.WriteLine(MakeTheStringGreat("leEeetcode"));
+            Console.WriteLine(MakeTheStringGreat("abBAcC"));
+            Console.WriteLine(MakeTheStringGreat("s"));
+        }
+
+        public static string MakeTheStringGreat(string s)
+        {
+            int n = s.Length;
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    if (s[i].ToString().ToUpper() == s[i + 1].ToString()
+                     && s[i].ToString() == s[i + 1].ToString().ToLower())
+                    {
+                        s = s.Remove(i + 1, 1);
+                        s = s.Remove(i, 1);
+                    }
+                    else if (s[i].ToString() == s[i + 1].ToString().ToUpper()
+                         && s[i].ToString().ToLower() == s[i + 1].ToString())
+                    {
+                        s = s.Remove(i + 1, 1);
+                        s = s.Remove(i, 1);
+                    }
+                }
+            }
+
+            return s;
+        }
+    }
+}
