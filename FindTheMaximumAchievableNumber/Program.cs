c9dd8b1fedb2d2b2ebe7f4930cb12d8f36b1f0aa
Deyan Diulgerov <chukstv@abv.bg>
2023-09-27 13:24:03 +0300

    Add files via upload

---
+++ FindTheMaximumAchievableNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheMaximumAchievableNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheMaximumAchievableNumber(4, 1));
+            Console.WriteLine(FindTheMaximumAchievableNumber(3, 2));
+        }
+
+        public static int FindTheMaximumAchievableNumber(int num, int t)
+            => num + (t * 2);
+    }
+}
