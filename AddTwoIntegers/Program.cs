b23d14b8866a1365a7448847110a9924cb1af7e0
Deyan Diulgerov <chukstv@abv.bg>
2023-09-27 13:45:34 +0300

    Add files via upload

---
+++ AddTwoIntegers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddTwoIntegers(12, 5));
+            Console.WriteLine(AddTwoIntegers(-10, 4));
+        }
+
+        public static int AddTwoIntegers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
