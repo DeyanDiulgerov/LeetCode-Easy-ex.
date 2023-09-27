0560938330d5632b66b50a975b72b5bbfbbd6d0a
Deyan Diulgerov <chukstv@abv.bg>
2023-09-27 14:58:06 +0300

    Add files via upload

---
+++ CountOfMatchesInTournament/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfMatchesInTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountOfMatchesInTournament(7));
+            Console.WriteLine(CountOfMatchesInTournament(14));
+        }
+
+        public static int CountOfMatchesInTournament(int n)
+        {
+            int allMathces = 0;
+            while (n > 1)
+            {
+                if (n % 2 == 0)
+                {
+                    allMathces += n / 2;
+                    n -= n / 2;
+                }
+                else
+                {
+                    allMathces += (n - 1) / 2;
+                    n -= (n - 1) / 2;
+                }
+            }
+            return allMathces;
+        }
+    }
+}
