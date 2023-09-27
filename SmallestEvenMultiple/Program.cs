fa69e57db15fead808645d26640c95452c11efab
Deyan Diulgerov <chukstv@abv.bg>
2023-09-27 13:43:13 +0300

    Add files via upload

---
+++ SmallestEvenMultiple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestEvenMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallestEvenMultiple(5));
+            Console.WriteLine(SmallestEvenMultiple(6));
+        }
+
+        public static int SmallestEvenMultiple(int n)
+        {
+            int permN = n;
+            while (true)
+            {
+                if (n % permN == 0 && n % 2 == 0)
+                    return n;
+
+                n++;
+            }
+        }
+    }
+}
