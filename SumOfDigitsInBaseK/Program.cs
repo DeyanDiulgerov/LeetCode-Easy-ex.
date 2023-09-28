5a6fc6ead0e83dd38bf750c82c30200cddaa7750
Deyan Diulgerov <chukstv@abv.bg>
2023-09-28 14:45:23 +0300

    Add files via upload

---
+++ SumOfDigitsInBaseK/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigitsInBaseK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfDigitsInBaseK(34, 6));
+            Console.WriteLine(SumOfDigitsInBaseK(10, 10));
+        }
+
+        public static int SumOfDigitsInBaseK(int n, int k)
+        {
+            var allNums = new List<double>();
+
+            double quotient = n;
+
+            while (quotient != 0)
+            {
+                var remaining = quotient % k;
+                quotient = Math.Floor(quotient / k);
+                allNums.Add(remaining);
+            }
+
+            allNums.Reverse();
+            var result = 0;
+
+            for (int i = 0; i < allNums.Count(); i++)
+            {
+                result += (int)allNums[i];
+            }
+
+            return result;
+        }
+    }
+}
