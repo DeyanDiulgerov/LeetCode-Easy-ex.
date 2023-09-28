b1cfc59bc78a7d6629916514a7cb311c36bbdfaf
Deyan Diulgerov <chukstv@abv.bg>
2023-09-28 14:27:29 +0300

    Add files via upload

---
+++ ANumberAfterADoubleReversal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANumberAfterADoubleReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ANumberAfterADoubleReversal(526));
+            Console.WriteLine(ANumberAfterADoubleReversal(1800));
+            Console.WriteLine(ANumberAfterADoubleReversal(0));
+        }
+
+        public static bool ANumberAfterADoubleReversal(int num)
+        {
+            var strNum = num.ToString();
+            var reversed1 = "";
+            var reversed2 = "";
+
+            for (int i = strNum.Length - 1; i >= 0; i--)
+                reversed1 += strNum[i];
+
+            var newReversed1 = int.Parse(reversed1);
+            var strNew = newReversed1.ToString();
+
+            for (int i = strNew.Length - 1; i >= 0; i--)
+                reversed2 += strNew[i];
+
+            var newReversed2 = int.Parse(reversed2);
+
+            if (newReversed2 == num)
+                return true;
+            else
+                return false;
+        }
+    }
+}
