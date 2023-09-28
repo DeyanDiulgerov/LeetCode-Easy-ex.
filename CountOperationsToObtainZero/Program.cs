a7f92c7a25badda0ea1ef9d51210cceb5dd497b0
Deyan Diulgerov <chukstv@abv.bg>
2023-09-28 15:05:05 +0300

    Add files via upload

---
+++ CountOperationsToObtainZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOperationsToObtainZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountOperationsToObtainZero(2, 3));
+            Console.WriteLine(CountOperationsToObtainZero(10, 10));
+        }
+
+        public static int CountOperationsToObtainZero(int num1, int num2)
+        {
+            int counter = 0;
+
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 > num2)
+                    num1 -= num2;
+                else if (num2 > num1)
+                    num2 -= num1;
+                else
+                {
+                    counter++;
+                    break;
+                }
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
