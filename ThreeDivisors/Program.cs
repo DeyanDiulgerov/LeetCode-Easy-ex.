5eae3de76ed499d17f9152992470f5d8225d81d4
Deyan Diulgerov <chukstv@abv.bg>
2023-09-29 19:59:08 +0300

    Add files via upload

---
+++ ThreeDivisors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ThreeDivisors(2));
+            Console.WriteLine(ThreeDivisors(4));
+        }
+
+        public static bool ThreeDivisors(int n)
+        {
+            int counter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    counter++;
+            }
+
+            if (counter == 3)
+                return true;
+            else
+                return false;
+        }
+    }
+}
