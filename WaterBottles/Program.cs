c30b39b1c2dea8651902ba18bea9f865c91d9111
Deyan Diulgerov <chukstv@abv.bg>
2023-09-29 15:27:37 +0300

    Add files via upload

---
+++ WaterBottles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WaterBottles(9, 3));
+            Console.WriteLine(WaterBottles(15, 4));
+        }
+
+        public static int WaterBottles(int numBottles, int numExchange)
+        {
+            var emptyBottles = 0;
+            var sum = numBottles;
+            emptyBottles = numBottles;
+
+            while (emptyBottles >= numExchange)
+            {
+                var test = emptyBottles / numExchange;
+                var bonusEmpty = emptyBottles % numExchange;
+                emptyBottles /= numExchange;
+                emptyBottles += bonusEmpty;
+
+                sum += test;
+            }
+
+            return sum;
+        }
+    }
+}
