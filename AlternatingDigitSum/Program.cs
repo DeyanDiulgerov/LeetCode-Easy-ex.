75abc7eac4bd9c628b600f4c3aa4b9a7d6ae3d82
Deyan Diulgerov <chukstv@abv.bg>
2023-09-29 13:50:22 +0300

    Add files via upload

---
+++ AlternatingDigitSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternatingDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlternatingDigitSum(521));
+            Console.WriteLine(AlternatingDigitSum(111));
+            Console.WriteLine(AlternatingDigitSum(886996));
+        }
+
+        public static int AlternatingDigitSum(int n)
+        {
+            var chNum = n.ToString().ToCharArray();
+            var positiveSum = 0;
+            var negativeSum = 0;
+
+            for (int i = 0; i < chNum.Length; i += 2)
+                positiveSum += chNum[i] - 48;
+            for (int i = 1; i < chNum.Length; i += 2)
+                negativeSum -= chNum[i] - 48;
+
+            return positiveSum + negativeSum;
+        }
+    }
+}
