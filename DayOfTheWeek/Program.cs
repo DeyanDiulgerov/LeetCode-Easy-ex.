5bbf0bcc99bfdf189f79f8e94f7e5a0d5b747056
Deyan Diulgerov <chukstv@abv.bg>
2023-09-29 20:36:53 +0300

    Add files via upload

---
+++ DayOfTheWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOfTheWeek(31, 8, 2019));
+            Console.WriteLine(DayOfTheWeek(18, 7, 1999));
+            Console.WriteLine(DayOfTheWeek(15, 8, 1993));
+        }
+
+        public static string DayOfTheWeek(int day, int month, int year)
+        {
+            return new DateTime(year, month, day).ToString("dddd");
+        }
+    }
+}
