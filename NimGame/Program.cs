e793f44d38890610b1c0890ad5f5297f003c833a
Deyan Diulgerov <chukstv@abv.bg>
2023-09-30 13:32:31 +0300

    Update Program.cs

--- NimGame/Program.cs
+++ NimGame/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace NimGame
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
+            Console.WriteLine(NimGame(7));
+            Console.WriteLine(NimGame(5));
+            Console.WriteLine(NimGame(4));
+            Console.WriteLine(NimGame(1));
+        }
+
+        public static bool NimGame(int n)
+        {
+            if (n <= 3)
+                return true;
+            if (n % 4 != 0)
+                return true;
+            else
+                return false;
         }
     }
 }
