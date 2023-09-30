81c0022415f290d6b99eb011f51cbaf1928b5136
Deyan Diulgerov <chukstv@abv.bg>
2023-09-30 13:47:43 +0300

    Add files via upload

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PalindromeNumber(121));
+            Console.WriteLine(PalindromeNumber(-121));
+            Console.WriteLine(PalindromeNumber(10));
+        }
+
+        public static bool PalindromeNumber(int x)
+        {
+            var reversed = "";
+            var chX = x.ToString();
+
+            for (int i = chX.Length - 1; i >= 0; i--)
+                reversed += chX[i];
+
+            if (chX == reversed)
+                return true;
+            else
+                return false;
+        }
+    }
+}
