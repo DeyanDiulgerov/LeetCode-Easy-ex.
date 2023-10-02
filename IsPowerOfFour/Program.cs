6fe1a0ee08c9350fd5ac60e9d593fca5e6d63925
Deyan Diulgerov <chukstv@abv.bg>
2023-10-02 15:29:34 +0300

    Add files via upload

---
+++ IsPowerOfFour/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPowerOfFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPowerOfFour(16));
+            Console.WriteLine(IsPowerOfFour(32));
+            Console.WriteLine(IsPowerOfFour(64));
+            Console.WriteLine(IsPowerOfFour(62));
+            Console.WriteLine(IsPowerOfFour(66));
+            Console.WriteLine(IsPowerOfFour(12));
+            Console.WriteLine(IsPowerOfFour(5));
+            Console.WriteLine(IsPowerOfFour(1));
+        }
+
+        public static bool IsPowerOfFour(int n)
+        {
+            for (long i = 1; i <= n; i *= 4)
+            {
+                if (i == n)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
