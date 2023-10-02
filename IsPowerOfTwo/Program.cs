d4f74698315ff0ab40a8d458506a1a6d67aeb597
Deyan Diulgerov <chukstv@abv.bg>
2023-10-02 19:09:02 +0300

    Add files via upload

---
+++ IsPowerOfTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPowerOfTwo(1));
+            Console.WriteLine(IsPowerOfTwo(16));
+            Console.WriteLine(IsPowerOfTwo(3));
+            Console.WriteLine(IsPowerOfTwo(4));
+            Console.WriteLine(IsPowerOfTwo(6));
+            Console.WriteLine(IsPowerOfTwo(8));
+            Console.WriteLine(IsPowerOfTwo(22));
+        }
+        public static bool IsPowerOfTwo(int n)
+        {
+            for (long i = 1; i <= n; i *= 2)
+            {
+                if (i == n)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
