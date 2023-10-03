28dfde6cd9ac90f86b45ee90ea720409333223d3
Deyan Diulgerov <chukstv@abv.bg>
2023-10-03 16:01:37 +0300

    Add files via upload

---
+++ CountSortedVowelStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSortedVowelStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSortedVowelStrings(1));
+            Console.WriteLine(CountSortedVowelStrings(2));
+            Console.WriteLine(CountSortedVowelStrings(10));
+            Console.WriteLine(CountSortedVowelStrings(33));
+        }
+        public static int CountSortedVowelStrings(int n)
+        {
+            return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+        }
+    }
+}
