361437e414fce2201ad1b863ff1f28f00e9218bd
Deyan Diulgerov <chukstv@abv.bg>
2023-10-05 15:13:19 +0300

    Add files via upload

---
+++ MinimumMovesToConvertString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumMovesToConvertString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumMovesToConvertString("OXOX"));
+            Console.WriteLine(MinimumMovesToConvertString("XXX"));
+            Console.WriteLine(MinimumMovesToConvertString("XXOX"));
+            Console.WriteLine(MinimumMovesToConvertString("OOOO"));
+        }
+        public static int MinimumMovesToConvertString(string s)
+        {
+            if (s.All(x => x == 'O'))
+                return 0;
+
+            int counter = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'O')
+                    continue;
+
+                counter++;
+                i += 2;
+            }
+            return counter;
+        }
+    }
+}
