af8b83048e0f255239fbef1052472ee5de372fa4
Deyan Diulgerov <chukstv@abv.bg>
2023-10-12 18:22:36 +0300

    Add files via upload

---
+++ NumberOfEquivalentDominoPairs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfEquivalentDominoPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dominoes1 = new int[][]
+            {
+                new int[] {1,2},
+                new int[] {2,1},
+                new int[] {3,4},
+                new int[] {5,6},
+            };
+            var dominoes2 = new int[][]
+          {
+                new int[] {1,2},
+                new int[] {1,2},
+                new int[] {1,1},
+                new int[] {1,2},
+                new int[] {2,2},
+          };
+
+            Console.WriteLine(NumberOfEquivalentDominoPairs(dominoes1));
+            Console.WriteLine(NumberOfEquivalentDominoPairs(dominoes2));
+        }
+
+        public static int NumberOfEquivalentDominoPairs(int[][] dominoes)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < dominoes.Length; i++)
+            {
+                for (int j = i + 1; j < dominoes.Length; j++)
+                {
+                    if (dominoes[i][0] == dominoes[j][0] && dominoes[i][1] == dominoes[j][1]
+                     || dominoes[i][0] == dominoes[j][1] && dominoes[i][1] == dominoes[j][0])
+                        counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
