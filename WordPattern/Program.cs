0a7a24df986bdb959f225381c12a95ee91f0b792
Deyan Diulgerov <chukstv@abv.bg>
2023-10-13 15:01:33 +0300

    Add files via upload

---
+++ WordPattern/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordPattern("abba", "dog dog dog dog"));
+            Console.WriteLine(WordPattern("abab", "dog cat cat dog"));
+            Console.WriteLine(WordPattern("aba", "cat cat cat dog"));
+            Console.WriteLine(WordPattern("abba", "dog cat cat dog"));
+            Console.WriteLine(WordPattern("abba", "dog cat cat fish"));
+            Console.WriteLine(WordPattern("aaaa", "dog cat cat dog"));
+        }
+
+        public static bool WordPattern(string pattern, string s)
+        {
+            var splittedS = s.Split(' ').ToArray();
+
+            if (pattern.Length != splittedS.Count())
+                return false;
+
+            var letterAndStringDict = new Dictionary<char, string>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!letterAndStringDict.ContainsKey(pattern[i]))
+                {
+                    if (letterAndStringDict.Any(x => x.Value == splittedS[i]))
+                        return false;
+
+                    letterAndStringDict.Add(pattern[i], splittedS[i]);
+                }
+                else
+                {
+                    if (letterAndStringDict[pattern[i]] != splittedS[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
