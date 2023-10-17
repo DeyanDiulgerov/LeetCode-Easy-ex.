fda7c18bc1de6359f6d3798da69e1b99e0df7ac5
Deyan Diulgerov <chukstv@abv.bg>
2023-10-17 19:12:41 +0300

    Add files via upload

---
+++ MaximumDifferenceBetweenIncreasingElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumDifferenceBetweenIncreasingElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaximumDifferenceBetweenIncreasingElements(new int[] { 7, 1, 5, 4 }));
+            Console.WriteLine(MaximumDifferenceBetweenIncreasingElements(new int[] { 9, 4, 3, 2 }));
+            Console.WriteLine(MaximumDifferenceBetweenIncreasingElements(new int[] { 1, 5, 2, 10 }));
+        }
+
+        public static int MaximumDifferenceBetweenIncreasingElements(int[] nums)
+        {
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        var newMax = nums[j] - nums[i];
+                        max = Math.Max(max, newMax);
+                    }
+                }
+            }
+
+            if (max > 0)
+                return max;
+            else
+                return -1;
+        }
+    }
+}
