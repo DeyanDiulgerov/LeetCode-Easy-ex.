9fa89999cf1a14780e13edbbc6c51fe9a7fee3c4
Deyan Diulgerov <chukstv@abv.bg>
2023-10-18 21:14:17 +0300

    Add files via upload

---
+++ LongestContinuousIncreasingSubsequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestContinuousIncreasingSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestContinuousIncreasingSubsequence(new int[] { 1, 3, 5, 7 }));
+            Console.WriteLine(LongestContinuousIncreasingSubsequence(new int[] { 1, 3, 5, 4, 7 }));
+            Console.WriteLine(LongestContinuousIncreasingSubsequence(new int[] { 2, 2, 2, 2, 2 }));
+        }
+
+        public static int LongestContinuousIncreasingSubsequence(int[] nums)
+        {
+            int max = 0;
+            int counter = 1;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                    counter++;
+                else
+                {
+                    max = Math.Max(max, counter);
+                    counter = 1;
+                }
+            }
+            max = Math.Max(max, counter);
+            return max;
+        }
+    }
+}
