f33cfb4356716ce57f0c8f8a8f326c1fdc56093b
Deyan Diulgerov <chukstv@abv.bg>
2023-10-19 14:17:55 +0300

    Add files via upload

---
+++ BinaryPrefixDivisibleBy5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryPrefixDivisibleBy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", BinaryPrefixDivisibleBy5(new int[] { 0, 1, 1 })));
+            Console.WriteLine(String.Join(",", BinaryPrefixDivisibleBy5(
+                new int[] { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1 })));
+            Console.WriteLine(String.Join(",", BinaryPrefixDivisibleBy5(new int[] { 1, 1, 1 })));
+        }
+
+        public static IList<bool> BinaryPrefixDivisibleBy5(int[] nums)
+        {
+            int num = 0;
+            var result = new List<bool>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                num = (num * 2 + nums[i]) % 10;
+                if (num % 5 == 0)
+                    result.Add(true);
+                else
+                    result.Add(false);
+            }
+
+            return result;
+        }
+    }
+}
