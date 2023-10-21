057871f4c2d9d4b7efb1163f52f068b3f0b7a9d9
Deyan Diulgerov <chukstv@abv.bg>
2023-10-21 15:21:26 +0300

    Add files via upload

---
+++ DetermineIfStringHalvesAreAlike/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetermineIfStringHalvesAreAlike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DetermineIfStringHalvesAreAlike("book"));
+            Console.WriteLine(DetermineIfStringHalvesAreAlike("textbook"));
+        }
+
+        public static bool DetermineIfStringHalvesAreAlike(string s)
+        {
+            var half = s.Length / 2;
+            var firstCounter = 0;
+            var secondCounter = 0;
+            var allVowels = new List<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            for (int i = 0; i < half; i++)
+                if (allVowels.Contains(s[i]))
+                    firstCounter++;
+
+            for (int i = half; i < s.Length; i++)
+                if (allVowels.Contains(s[i]))
+                    secondCounter++;
+
+            if (firstCounter == secondCounter)
+                return true;
+
+            return false;
+        }
+    }
+}
