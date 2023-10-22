89a6f4e259de698d1271a1229f80b111323421f3
Deyan Diulgerov <chukstv@abv.bg>
2023-10-22 14:16:14 +0300

    Add files via upload

---
+++ RotateString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RotateString("abcde", "cdeab"));
+            Console.WriteLine(RotateString("abcde", "abced"));
+        }
+
+        public static bool RotateString(string s, string goal)
+        {
+            var usedStrings = new List<string>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (usedStrings.Contains(s))
+                    return false;
+                if (s == goal)
+                    return true;
+                else
+                    usedStrings.Add(s);
+
+                var curr = s[0];
+                s = s.Remove(0, 1);
+                s += curr;
+            }
+            if (s == goal)
+                return true;
+            else
+                return false;
+        }
+    }
+}
