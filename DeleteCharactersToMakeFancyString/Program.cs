4b6739a1ac39bc027d27a1a2716b55695a6d0e2b
Deyan Diulgerov <chukstv@abv.bg>
2023-10-22 13:52:43 +0300

    Add files via upload

---
+++ DeleteCharactersToMakeFancyString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteCharactersToMakeFancyString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DeleteCharactersToMakeFancyString("aaabaaaa"));
+            Console.WriteLine(DeleteCharactersToMakeFancyString("leeetcode"));
+            Console.WriteLine(DeleteCharactersToMakeFancyString("aab"));
+        }
+
+        public static string DeleteCharactersToMakeFancyString(string s)
+        {
+            var sb = new StringBuilder();
+            sb.Append(s[0]);
+            int counter = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (sb[sb.Length - 1] == s[i])
+                    counter++;
+                else
+                    counter = 1;
+
+                if (counter < 3)
+                    sb.Append(s[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
