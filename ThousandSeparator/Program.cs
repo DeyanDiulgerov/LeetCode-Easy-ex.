a34ca71cf1cb72a1aa6e1b5540546a637ba9933b
Deyan Diulgerov <chukstv@abv.bg>
2023-10-23 14:04:28 +0300

    Add files via upload

---
+++ ThousandSeparator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThousandSeparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ThousandSeparator(987));
+            Console.WriteLine(ThousandSeparator(1234));
+            Console.WriteLine(ThousandSeparator(12345));
+            Console.WriteLine(ThousandSeparator(2120345));
+        }
+
+        public static string ThousandSeparator(int n)
+        {
+            var str = n.ToString();
+            int counter = 1;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (counter == 3 && i != 0)
+                {
+                    str = str.Insert(i, ".");
+                    counter = 0;
+                }
+
+                counter++;
+            }
+
+            return str;
+        }
+    }
+}
