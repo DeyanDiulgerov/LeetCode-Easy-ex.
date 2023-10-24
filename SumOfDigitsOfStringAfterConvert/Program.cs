1de30f6b41672ebb4033c35d07ac40f4d0a70993
Deyan Diulgerov <chukstv@abv.bg>
2023-10-24 15:16:00 +0300

    Add files via upload

---
+++ SumOfDigitsOfStringAfterConvert/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigitsOfStringAfterConvert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfDigitsOfStringAfterConvert("iiii", 1));
+            Console.WriteLine(SumOfDigitsOfStringAfterConvert("leetcode", 2));
+            Console.WriteLine(SumOfDigitsOfStringAfterConvert("zbax", 10));
+        }
+        public static int SumOfDigitsOfStringAfterConvert(string s, int k)
+        {
+            var newStr = "";
+            var result = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < s.Length; j++)
+                        newStr += s[j] - 96;
+
+                    for (int l = 0; l < newStr.Length; l++)
+                        result += newStr[l] - 48;
+                }
+                else
+                {
+                    newStr = result.ToString();
+                    result = 0;
+
+                    for (int m = 0; m < newStr.Length; m++)
+                        result += newStr[m] - 48;
+                }
+            }
+
+            return result;
+        }
+    }
+}
