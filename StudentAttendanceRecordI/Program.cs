8e83d910528cf1ac198231af547943554a3d8f10
Deyan Diulgerov <chukstv@abv.bg>
2023-10-25 15:19:08 +0300

    Add files via upload

---
+++ StudentAttendanceRecordI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAttendanceRecordI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StudentAttendanceRecordI("LALL"));
+            Console.WriteLine(StudentAttendanceRecordI("PPALLP"));
+            Console.WriteLine(StudentAttendanceRecordI("PPALLL"));
+        }
+        public static bool StudentAttendanceRecordI(string s)
+        {
+            if (s.Count(x => x == 'A') >= 2)
+                return false;
+
+            var test = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'L')
+                    test += s[i];
+                else
+                    test = "";
+
+                if (test.Count(x => x == 'L') >= 3)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
