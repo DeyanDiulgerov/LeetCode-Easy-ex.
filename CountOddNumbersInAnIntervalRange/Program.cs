9789ad1e300c04dbb7ae2eb7497a017b5ac82f59
Deyan Diulgerov <chukstv@abv.bg>
2023-11-07 15:15:44 +0200

    Add files via upload

---
+++ CountOddNumbersInAnIntervalRange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOddNumbersInAnIntervalRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountOddNumbersInAnIntervalRange(3, 7));
+            Console.WriteLine(CountOddNumbersInAnIntervalRange(9, 10));
+        }
+        public static int CountOddNumbersInAnIntervalRange(int low, int high)
+        {
+            int count = 0;
+            for (long i = low; i <= high; i++)
+            {
+                if (i % 2 != 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
