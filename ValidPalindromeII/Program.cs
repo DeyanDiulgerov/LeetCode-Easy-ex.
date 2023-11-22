054d70a7604bcd3450762416bf21212a26b74a78
Deyan Diulgerov <chukstv@abv.bg>
2023-11-22 19:21:59 +0200

    Add files via upload

---
+++ ValidPalindromeII/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidPalindromeII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidPalindromeII("cxcaac"));
+            Console.WriteLine(ValidPalindromeII("eedede"));
+            Console.WriteLine(ValidPalindromeII("eedede"));
+            Console.WriteLine(ValidPalindromeII("aba"));
+            Console.WriteLine(ValidPalindromeII("abca"));
+            Console.WriteLine(ValidPalindromeII("abc"));
+        }
+
+        public static bool ValidPalindromeII(string s)
+        {
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return IsPalindrome(s, left + 1, right)
+                        || IsPalindrome(s, left, right - 1);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        private static bool IsPalindrome(string s, int left, int right)
+        {
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
