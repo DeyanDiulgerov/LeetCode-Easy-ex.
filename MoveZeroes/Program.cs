c07d5ddd2275623f827deaeee95331cb8b432c05
Deyan Diulgerov <chukstv@abv.bg>
2023-11-22 16:56:59 +0200

    Update Program.cs

--- MoveZeroes/Program.cs
+++ MoveZeroes/Program.cs
@@ -10,26 +10,34 @@ namespace MoveZeroes
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(String.Join(",", MoveZeroes(new int[] { 2, 1 })));
+            Console.WriteLine(String.Join(",", MoveZeroes(new int[] { 4, 2, 4, 0, 0, 3, 0, 5, 1, 0 })));
+            Console.WriteLine(String.Join(",", MoveZeroes(new int[] { 0, 0 })));
+            Console.WriteLine(String.Join(",", MoveZeroes(new int[] { 0, 1, 0, 3, 12 })));
+            Console.WriteLine(String.Join(",", MoveZeroes(new int[] { 0 })));
         }
 
-        public static void MoveZeroes(int[] nums)
+      //public static void MoveZeroes(int[] nums)
+        public static int[] MoveZeroes(int[] nums)
         {
-            var index = 0;
-            for (int i = 0; i < nums.Length; i++)
+            int left = 0, right = 1;
+            while (right < nums.Length)
             {
-                if (nums[i] == 0)
-                    continue;
+                // While nums[left] != 0, nums[right] == 0;
+                while (left < nums.Length && nums[left] != 0)
+                    left++;
+                while (right <= left || (right < nums.Length && nums[right] == 0))
+                    right++;
+                if (right >= nums.Length)
+                    break;
 
-                nums[index] = nums[i];
-
-                if (index != i)
-                    nums[i] = 0;
-
-                index++;
+                var temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right++;
             }
-
-            return;
+            return nums;
         }
     }
 }
