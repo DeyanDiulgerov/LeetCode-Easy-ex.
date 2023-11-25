5b1e2c20880c1ea366a4c109dc302983736d2a7c
Deyan Diulgerov <chukstv@abv.bg>
2023-11-25 16:29:58 +0200

    Update Program.cs

--- IsSubsequence/Program.cs
+++ IsSubsequence/Program.cs
@@ -19,16 +19,15 @@ namespace IsSubsequence
 
         public static bool IsSubsequence(string s, string t)
         {
-            if (s == String.Empty)
-                return true;
+            int left = 0, right = 0;
+            while (left < s.Length && right < t.Length)
+            {
+                if (s[left] == t[right])
+                    left++;
 
-            int count = 0;
-
-            for (int i = 0; i < t.Length; i++)
-                if (count < s.Length && s[count] == t[i])
-                    count++;
-
-            return count == s.Length;
+                right++;
+            }
+            return left == s.Length;
         }
     }
 }
