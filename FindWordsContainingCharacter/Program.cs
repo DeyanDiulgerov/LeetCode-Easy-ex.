e420890f630613562e948c63dec6e9a389c588dc
Deyan Diulgerov <chukstv@abv.bg>
2023-11-28 19:21:45 +0200

    Add files via upload

---
+++ FindWordsContainingCharacter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindWordsContainingCharacter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",",
+                FindWordsContainingCharacter(new string[] { "leet", "code" }, 'e')));
+            Console.WriteLine(String.Join(",",
+                FindWordsContainingCharacter(new string[] { "abc", "bcd", "aaaa", "cbc" }, 'a')));
+            Console.WriteLine(String.Join(",",
+                FindWordsContainingCharacter(new string[] { "abc", "bcd", "aaaa", "cbc" }, 'z')));
+        }
+        public static IList<int> FindWordsContainingCharacter(string[] words, char x)
+        {
+            var resultList = new List<int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(x))
+                    resultList.Add(i);
+            }
+            return resultList;
+        }
+    }
+}
