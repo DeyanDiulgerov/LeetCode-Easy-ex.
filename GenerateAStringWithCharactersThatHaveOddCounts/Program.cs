a5e1a8968971bb35746b4aad6af8959ec1006789
Deyan Diulgerov <chukstv@abv.bg>
2023-12-01 14:41:56 +0200

    Add files via upload

---
+++ GenerateAStringWithCharactersThatHaveOddCounts/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateAStringWithCharactersThatHaveOddCounts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenerateAStringWithCharactersThatHaveOddCounts(1));
+            Console.WriteLine(GenerateAStringWithCharactersThatHaveOddCounts(4));
+            Console.WriteLine(GenerateAStringWithCharactersThatHaveOddCounts(2));
+            Console.WriteLine(GenerateAStringWithCharactersThatHaveOddCounts(7));
+        }
+
+        public static string GenerateAStringWithCharactersThatHaveOddCounts(int n)
+        {
+            if (n == 1)
+                return "a";
+            var result = "";
+
+            if (n % 2 == 0)
+            {
+                for (int i = 0; i < n - 1; i++)
+                    result += 'a';
+
+                result += 'b';
+            }
+            else
+            {
+                for (int i = 0; i < n - 2; i++)
+                    result += 'a';
+
+                result += 'b';
+                result += 'c';
+            }
+            return result;
+        }
+    }
+}
