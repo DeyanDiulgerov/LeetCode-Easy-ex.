d6d2e1c3ff0119b05ba33d1cbf1e457ec099f0ad
Deyan Diulgerov <chukstv@abv.bg>
2023-12-01 16:00:58 +0200

    Add files via upload

---
+++ OccurrencesAfterBigram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OccurrencesAfterBigram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", OccurrencesAfterBigram
+                ("alice is a good girl she is a good student", "a", "good")));
+            Console.WriteLine(String.Join(",", OccurrencesAfterBigram
+                ("we will we will rock you", "we", "will")));
+        }
+
+        public static string[] OccurrencesAfterBigram(string text, string first, string second)
+        {
+            var resultList = new List<string>();
+            var splitted = text.Split(' ');
+
+            for (int i = 0; i < splitted.Length - 2; i++)
+            {
+                if (splitted[i] == first && splitted[i + 1] == second)
+                    resultList.Add(splitted[i + 2]);
+            }
+            return resultList.ToArray();
+        }
+    }
+}
