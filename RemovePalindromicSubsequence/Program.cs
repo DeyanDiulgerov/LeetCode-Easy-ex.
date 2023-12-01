0305a2d571f883da3e99e5748654eb21422ae3a5
Deyan Diulgerov <chukstv@abv.bg>
2023-12-01 14:31:24 +0200

    Add files via upload

---
+++ RemovePalindromicSubsequence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovePalindromicSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemovePalindromicSubsequence("bbaabaaa"));
+            Console.WriteLine(RemovePalindromicSubsequence("ababa"));
+            Console.WriteLine(RemovePalindromicSubsequence("abb"));
+            Console.WriteLine(RemovePalindromicSubsequence("baabb"));
+        }
+
+        public static int RemovePalindromicSubsequence(string s)
+        {
+            // s consists ONLY of 'a' and 'b';
+            // and we can remove SUBSEQUENCE;
+            // so answer will always be 1 or 2;
+            int left = 0, right = s.Length / 2;
+            if (s.Length % 2 == 0)
+                left = s.Length / 2 - 1;
+            else
+                left = s.Length / 2;
+
+            int counter = 1;
+            while (left >= 0 && right < s.Length)
+            {
+                if (s[left] != s[right])
+                    counter++;
+
+                left--;
+                right++;
+            }
+            return counter == 1 ? 1 : 2;
+        }
+    }
+}
