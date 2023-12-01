28cb98eb6f85f9d4c1c7fc658b268edc02b89cc9
Deyan Diulgerov <chukstv@abv.bg>
2023-12-01 15:31:35 +0200

    Add files via upload

---
+++ CheckIfAWordOccursAsAPrefixOfAnyWordInASentence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfAWordOccursAsAPrefixOfAnyWordInASentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckIfAWordOccursAsAPrefixOfAnyWordInASentence("hellohello hellohellohello", "ell"));
+            Console.WriteLine(CheckIfAWordOccursAsAPrefixOfAnyWordInASentence("i love eating burger", "burg"));
+            Console.WriteLine(CheckIfAWordOccursAsAPrefixOfAnyWordInASentence("this problem is an easy problem", "pro"));
+            Console.WriteLine(CheckIfAWordOccursAsAPrefixOfAnyWordInASentence("i am tired", "you"));
+        }
+        public static int CheckIfAWordOccursAsAPrefixOfAnyWordInASentence(string sentence, string searchWord)
+        {
+            var splitted = sentence.Split(' ');
+
+            if (!splitted.Any(x => x.Contains(searchWord)))
+                return -1;
+
+            for (int i = 0; i < splitted.Length; i++)
+                if (splitted[i].StartsWith(searchWord))
+                    return i + 1;
+
+            return -1;
+        }
+    }
+}
