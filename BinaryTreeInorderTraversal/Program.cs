39f205c83f09d5ff26695a80b1f00a2d01d147b6
Deyan Diulgerov <chukstv@abv.bg>
2023-12-09 14:25:49 +0200

    Add files via upload

---
+++ BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeInorderTraversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (stack.Count != 0 || root != null)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    root = stack.Pop();
+                    result.Add(root.val);
+                    root = root.right;
+                }
+            }
+            return result;
+        }
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    }
+}
