32fab843991b9c22ccf5e6fa1c2dcec50b287358
Deyan Diulgerov <chukstv@abv.bg>
2023-12-09 17:12:26 +0200

    2 ways

--- SortedSquaresInArray/Program.cs
+++ SortedSquaresInArray/Program.cs
@@ -13,7 +13,31 @@ namespace SortedSquaresInArray
             Console.WriteLine(String.Join(",", SortedSquaresInArray(new int[] { -4, -1, 0, 3, 10 })));
             Console.WriteLine(String.Join(",", SortedSquaresInArray(new int[] { -7, -3, 2, 3, 11 })));
         }
+        //Two Pointers
+         public static int[] SortedSquares(int[] nums)
+        {
+            int n = nums.Length;
+            int left = 0, right = n - 1;
+            var resultArr = new int[n];
+            int index = n - 1;
 
+            while (left <= right)
+            {
+                if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+                {
+                    resultArr[index] = nums[left] * nums[left];
+                    left++;
+                }
+                else
+                {
+                    resultArr[index] = nums[right] * nums[right];
+                    right--;
+                }
+                index--;
+            }
+            return resultArr;
+        }
+        // Trivial Way
         public static int[] SortedSquaresInArray(int[] nums)
         {
             var retList = new List<int>();
@@ -27,6 +51,6 @@ namespace SortedSquaresInArray
 
             return retList.ToArray();
         }
-
+        
     }
 }
