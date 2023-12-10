280f92d76a88b4d8fcfb433553c94f740b021ba2
Deyan Diulgerov <chukstv@abv.bg>
2023-12-10 14:32:38 +0200

    Added Two Pointers approach

--- Remove Duplicates from Sorted Array/Program.cs
+++ Remove Duplicates from Sorted Array/Program.cs
@@ -13,7 +13,29 @@ namespace RemoveDuplicatesFromSortedArray
             RemoveDuplicates(nums);
 
         }
+        // My Way - Two Pointers
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int left = 0, right = 0;
+            int indexCount = 0;
+            int distinctCount = nums.Distinct().Count();
 
+            while (left < nums.Length - 1)
+            {
+                left = right;
+
+                while (right < nums.Length - 1 && nums[left] == nums[right])
+                    right++;
+
+                nums[indexCount] = nums[left];
+                indexCount++;
+
+                if (indexCount >= distinctCount)
+                    break;
+            }
+            return nums.Take(distinctCount).Count();
+        }
+        // NOT mine - Leetcode solution
         public static int RemoveDuplicates(int[] nums)
         {
             var count = (nums.Length == 0 ? 0 : 1);
