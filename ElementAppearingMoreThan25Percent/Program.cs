7add7e7cf3f2ea398b0081bd3f6a13cc70feb8a4
Deyan Diulgerov <chukstv@abv.bg>
2023-12-11 13:28:47 +0200

    2nd Way beats 96.61%

--- ElementAppearingMoreThan25Percent/Program.cs
+++ ElementAppearingMoreThan25Percent/Program.cs
@@ -13,7 +13,24 @@ namespace ElementAppearingMoreThan25Percent
             Console.WriteLine(ElementAppearingMoreThan25Percent(new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 }));
             Console.WriteLine(ElementAppearingMoreThan25Percent(new int[] { 1, 1 }));
         }
+        // 2nd Way beats 96.61%
+        public static int FindSpecialInteger(int[] arr)
+        {
+            double targetFreq = arr.Length * 0.25;
+            var numAndFrequencyMap = new Dictionary<int, int>();
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!numAndFrequencyMap.ContainsKey(arr[i]))
+                    numAndFrequencyMap.Add(arr[i], 1);
+                else
+                    numAndFrequencyMap[arr[i]]++;
+
+                if (numAndFrequencyMap[arr[i]] > targetFreq)
+                    return arr[i];
+            }
+            return -1;
+        }
         public static int ElementAppearingMoreThan25Percent(int[] arr)
         {
             var numAndAppeareance = new Dictionary<int, int>();
