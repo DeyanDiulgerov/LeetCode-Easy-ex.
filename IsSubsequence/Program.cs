2430ebb0a7bca2d31c5f2484adcfdb1d7aef3ed7
Deyan Diulgerov <chukstv@abv.bg>
2023-12-11 15:04:18 +0200

    2nd Way beats 99.73% Runtime & 84.67% Memory

--- IsSubsequence/Program.cs
+++ IsSubsequence/Program.cs
@@ -16,7 +16,19 @@ namespace IsSubsequence
             Console.WriteLine(IsSubsequence("abc", "ahbgdc"));
             Console.WriteLine(IsSubsequence("axc", "ahbgdc"));
         }
-
+        // 2nd Way beats 99.73% Runtime & 84.67% Memory
+        public static bool IsSubsequence(string s, string t)
+        {
+            int left = 0;
+            for (int right = 0; right < t.Length; right++)
+            {
+                if (left == s.Length)
+                    return true;
+                if (s[left] == t[right])
+                    left++;
+            }
+            return left == s.Length;
+        }
         public static bool IsSubsequence(string s, string t)
         {
             int left = 0, right = 0;
