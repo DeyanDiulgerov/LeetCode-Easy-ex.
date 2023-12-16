4e10070732ad4ff608bb23f24f874728a7cd180f
Deyan Diulgerov <chukstv@abv.bg>
2023-12-16 14:16:29 +0200

    Add files via upload

---
+++ DesignHashSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class MyHashSet
+        {
+            HashSet<int> set = null;
+            public MyHashSet()
+            {
+                set = new HashSet<int>();
+            }
+
+            public void Add(int key)
+            {
+                if (!set.Contains(key))
+                    set.Add(key);
+            }
+
+            public void Remove(int key)
+            {
+                if (set.Contains(key))
+                    set.Remove(key);
+            }
+
+            public bool Contains(int key)
+            {
+                return set.Contains(key);
+            }
+        }
+    }
+}
