abc5c48c2d71687249e88cf17b85866209e0152f
Deyan Diulgerov <chukstv@abv.bg>
2023-12-24 15:14:42 +0200

    Add files via upload

---
+++ MinimumChangesToMakeAlternatingBinaryString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumChangesToMakeAlternatingBinaryString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumChangesToMakeAlternatingBinaryString("0100"));
+            Console.WriteLine(MinimumChangesToMakeAlternatingBinaryString("10"));
+            Console.WriteLine(MinimumChangesToMakeAlternatingBinaryString("1111"));
+        }
+        public static int MinimumChangesToMakeAlternatingBinaryString(string s)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (s[i] == '0')
+                        counter++;
+                }
+                else
+                {
+                    if (s[i] == '1')
+                        counter++;
+                }
+            }
+            return Math.Min(counter, s.Length - counter);
+        }
+    }
+}
