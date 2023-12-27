a28d6a0ee425c6de2778e282d01533a4e6af4f31
Deyan Diulgerov <chukstv@abv.bg>
2023-12-27 20:23:48 +0200

    Add files via upload

---
+++ NumberOfRecentCalls/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfRecentCalls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RecentCounter counter = new RecentCounter();
+            counter.Ping(1);
+            counter.Ping(100);
+            counter.Ping(3001);
+            counter.Ping(3002);
+        }
+        public class RecentCounter
+        {
+            Queue<int> allRequestsQueue = new Queue<int>();
+            public RecentCounter()
+            {
+                allRequestsQueue = new Queue<int>();
+            }
+
+            public int Ping(int t)
+            {
+                allRequestsQueue.Enqueue(t);
+                while (allRequestsQueue.Peek() < t - 3000)
+                {
+                    allRequestsQueue.Dequeue();
+                }
+                return allRequestsQueue.Count();
+            }
+        }
+    }
+}
