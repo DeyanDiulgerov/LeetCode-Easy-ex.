926b4692d8e3a61e258c82cdb0827ba61ec77f2c
Deyan Diulgerov <chukstv@abv.bg>
2024-01-03 17:13:35 +0200

    Added 2nd approach - Two Pointers

--- RemoveElement/Program.cs
+++ RemoveElement/Program.cs
@@ -12,8 +12,31 @@ namespace RemoveElement
         {
             Console.WriteLine(RemoveElement(new int[] { 3, 2, 2, 3 }, 3));
             Console.WriteLine(RemoveElement(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2));
+            Console.WriteLine(RemoveElementTwoPointers(new int[] { 3, 2, 2, 3 }, 3));
+            Console.WriteLine(RemoveElementTwoPointers(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2));
         }
+        //Two pointers
+        public static int RemoveElementTwoPointers(int[] nums, int val)
+        {
+            int left = 0, right = 0;
 
+            while (right < nums.Length)
+            {
+                right = left + 1;
+                while (left < nums.Length && nums[left] != val)
+                    left++;
+                while (right <= left || right < nums.Length && nums[right] == val)
+                    right++;
+                if (left >= right || right >= nums.Length)
+                    break;
+
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+            return nums.Count(x => x != val);
+        }
         public static int RemoveElement(int[] nums, int val)
         {
             var index = 0;
