b8363a9ba9df93642756cbade59a11026768b03c
Deyan Diulgerov <chukstv@abv.bg>
2024-01-19 19:44:12 +0200

    Added 2nd Way Fibonacci Number with Dp

--- FibonacciNumberAndSequence/Program.cs
+++ FibonacciNumberAndSequence/Program.cs
@@ -34,7 +34,22 @@ namespace FibonacciNumberAndSequence
             Console.WriteLine($"-------------------------------------------------------------------------------\n\n");
 
         }
+        // DP
+        public static int Fib(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1 || n == 2) return 1;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
 
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
         public static int FibonacciNumber(int number)
         {
             // Fibonacci Series 10
