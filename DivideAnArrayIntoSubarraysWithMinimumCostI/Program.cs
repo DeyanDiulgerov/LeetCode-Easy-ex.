8bf5cd9d3ef6d57a0c576253e5b4056a141b393a
Deyan Diulgerov <chukstv@abv.bg>
2024-01-26 13:13:13 +0200

    Add files via upload

---
+++ DivideAnArrayIntoSubarraysWithMinimumCostI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideAnArrayIntoSubarraysWithMinimumCostI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivideAnArrayIntoSubarraysWithMinimumCostI(new int[] { 1, 2, 3, 12 }));
+            Console.WriteLine(DivideAnArrayIntoSubarraysWithMinimumCostI(new int[] { 5, 4, 3 }));
+            Console.WriteLine(DivideAnArrayIntoSubarraysWithMinimumCostI(new int[] { 10, 3, 1, 1 }));
+        }
+        public static int DivideAnArrayIntoSubarraysWithMinimumCostI(int[] nums)
+        {
+            int result = nums[0];
+            var listed = new List<int>(nums);
+            listed.Remove(listed[0]);
+            result += listed.Min();
+            listed.Remove(listed.Min());
+            result += listed.Min();
+            listed.Remove(listed.Min());
+
+            return result;
+        }
+    }
+}
