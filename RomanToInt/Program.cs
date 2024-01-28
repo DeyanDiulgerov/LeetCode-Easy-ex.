1fce0e439f8a6058048019d9e422e3fa59afbdc3
Deyan Diulgerov <chukstv@abv.bg>
2024-01-28 21:21:13 +0200

    2nd Interesting approach
    Better performance

--- RomanToInt/Program.cs
+++ RomanToInt/Program.cs
@@ -13,7 +13,30 @@ namespace Testovo
             var input = Console.ReadLine();
             Console.WriteLine(RomanToInt(input));
         }
+        /2nd Way
+         public static int RomanToInt(string s)
+        {
+            var map = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
 
+            int result = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if(i == s.Length - 1 || map[s[i]] >= map[s[i + 1]])
+                    result += map[s[i]];
+                else
+                    result -= map[s[i]];
+            }
+            return result;
+        }
         public static int RomanToInt(string input)
         {
             if (input == null || input.Length == 0)
