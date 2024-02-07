b6115796d77029c3c1030780ed90b08a71604ffc
Deyan Diulgerov <chukstv@abv.bg>
2024-02-07 15:45:25 +0200

    2nd Better way
    2nd Way - using HashMap better performance longer code

--- RansomNote/Program.cs
+++ RansomNote/Program.cs
@@ -14,7 +14,32 @@ namespace RansomNote
             Console.WriteLine(RansomNote("a", "b"));
             Console.WriteLine(RansomNote("aa", "aab"));
         }
-
+        //2nd Better way
+        public static bool RansomNote(string ransomNote, string magazine)
+        {
+            var map1 = new Dictionary<char, int>();
+            var map2 = new Dictionary<char, int>();
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (!map1.ContainsKey(ransomNote[i]))
+                    map1.Add(ransomNote[i], 1);
+                else
+                    map1[ransomNote[i]]++;
+            }
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                if (!map2.ContainsKey(magazine[i]))
+                    map2.Add(magazine[i], 1);
+                else
+                    map2[magazine[i]]++;
+            }
+            foreach (var kvp in map1)
+            {
+                if (!map2.ContainsKey(kvp.Key) || map2[kvp.Key] < kvp.Value)
+                    return false;
+            }
+            return true;
+        }
         public static bool RansomNote(string ransomNote, string magazine)
         {
             var permNote = ransomNote;
