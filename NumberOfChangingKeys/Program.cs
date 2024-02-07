09c4cb7d915729aeda315b527ac686a6663b72a8
Deyan Diulgerov <chukstv@abv.bg>
2024-02-07 14:50:35 +0200

    Add files via upload

---
+++ NumberOfChangingKeys/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfChangingKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfChangingKeys("aAbBacC"));
+            Console.WriteLine(NumberOfChangingKeys("aAbBcC"));
+            Console.WriteLine(NumberOfChangingKeys("AaAaAaaA"));
+        }
+        public static int NumberOfChangingKeys(string s)
+        {
+            int left = 0, right = 0, count = 0;
+            while (right < s.Length)
+            {
+                right = left + 1;
+                while (right < s.Length && s[left].ToString().ToLower() == s[right].ToString().ToLower())
+                    right++;
+                if (right >= s.Length)
+                    break;
+                count++;
+                left = right;
+            }
+            return count;
+        }
+    }
+}
