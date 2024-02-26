40f14a3c9e002fbdfa7358e4beb0c03ac8203672
Deyan Diulgerov <chukstv@abv.bg>
2024-02-26 13:37:14 +0200

    Add files via upload

---
+++ SearchInABinarySearchTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInABinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+            if (root.val == val)
+                return root;
+            if (root.val < val)
+                return SearchBST(root.right, val);
+
+            return SearchBST(root.left, val);
+        }
+    }
+}
