c3b1ad6fcd00fbf4e2a3bbb0649ad6b1775bf9b9
Deyan Diulgerov <chukstv@abv.bg>
2024-02-26 12:39:40 +0200

    Add files via upload

---
+++ SameTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            if (p == null || q == null)
+                return false;
+            if (p.val != q.val)
+                return false;
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
