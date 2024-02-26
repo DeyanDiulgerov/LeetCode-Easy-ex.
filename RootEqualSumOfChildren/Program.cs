354a9beb9c75c4a0e935f64f49a024cc698e6d7f
Deyan Diulgerov <chukstv@abv.bg>
2024-02-26 13:30:52 +0200

    Add files via upload

---
+++ RootEqualSumOfChildren/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootEqualSumOfChildren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public bool CheckTree(TreeNode root)
+        {
+            return root.val == root.left.val + root.right.val;
+        }
+    }
+}
