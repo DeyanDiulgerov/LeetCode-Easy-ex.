0f0c6c5fd6e4602db9fa8a23daad48c716198dae
Deyan Diulgerov <chukstv@abv.bg>
2024-02-29 12:14:49 +0200

    Add files via upload

---
+++ NumberofEvenAndOddBits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberofEvenAndOddBits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", NumberofEvenAndOddBits(17)));
+            Console.WriteLine(String.Join(",", NumberofEvenAndOddBits(2)));
+        }
+        public static int[] NumberofEvenAndOddBits(int n)
+        {
+            string str = Convert.ToString(n, 2);
+            str = String.Concat(str.Reverse());
+            Console.WriteLine(str);
+            int evenCount = 0, oddCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0 && i != 1 && str[i] == '1')
+                    evenCount++;
+                else if (i % 2 != 0 && str[i] == '1')
+                    oddCount++;
+            }
+            return new int[] { evenCount, oddCount };
+        }
+    }
+}
