97ba4a54e30d41330893b6ec976cf8c4a80fba86
Deyan Diulgerov <chukstv@abv.bg>
2024-02-29 12:07:34 +0200

    Add files via upload

---
+++ PassThePillow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassThePillow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PassThePillow(4, 5));
+            Console.WriteLine(PassThePillow(3, 2));
+        }
+        public static int PassThePillow(int n, int time)
+        {
+            int count = 1;
+            bool shouldReverse = false;
+            while (time > 0)
+            {
+                if (count == n)
+                    shouldReverse = true;
+                else if (count == 1)
+                    shouldReverse = false;
+                if (shouldReverse)
+                    count--;
+                else
+                    count++;
+
+                time--;
+            }
+            return count;
+        }
+    }
+}
