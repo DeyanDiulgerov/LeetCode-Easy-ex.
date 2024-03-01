05a4404d6d65f0004409f3a25315fcfded99b321
Deyan Diulgerov <chukstv@abv.bg>
2024-03-01 12:17:00 +0200

    Add files via upload

---
+++ GreatestCommonDevisorOfStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestCommonDevisorOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GreatestCommonDevisorOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"));
+            Console.WriteLine(GreatestCommonDevisorOfStrings("ABCABC", "ABC"));
+            Console.WriteLine(GreatestCommonDevisorOfStrings("ABABAB", "ABAB"));
+            Console.WriteLine(GreatestCommonDevisorOfStrings("LEET", "CODE"));
+        }
+        public static string GreatestCommonDevisorOfStrings(string str1, string str2)
+        {
+            if (str1.Length == 0 || str2.Length == 0
+             || str1 + str2 != str2 + str1)
+                return "";
+
+            int shortest = str1.Length;
+            int prevRemainder = str2.Length;
+            int remainder = shortest % prevRemainder;
+            while (remainder != 0)
+            {
+                shortest = prevRemainder;
+                prevRemainder = remainder;
+                remainder = shortest % prevRemainder;
+            }
+            return str1.Substring(0, prevRemainder);
+        }
+    }
+}
---
+++ GreatestCommonDevisorOfStrings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GreatestCommonDevisorOfStrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GreatestCommonDevisorOfStrings")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0c21a56-38d5-4870-b017-165902f4564b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
