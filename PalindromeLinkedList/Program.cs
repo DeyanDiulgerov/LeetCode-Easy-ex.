becccd46fa4eb03d37a91ed02811b46ca85b9c60
Deyan Diulgerov <chukstv@abv.bg>
2024-03-07 14:10:05 +0200

    Add files via upload

---
+++ PalindromeLinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public bool IsPalindrome(ListNode head)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (head != null)
+            {
+                sb.Append(head.val);
+                head = head.next;
+            }
+            string str = sb.ToString();
+            return str.Reverse().SequenceEqual(str);
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
