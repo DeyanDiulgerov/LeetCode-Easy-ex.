8c2724755d90124a3be5cf4f5a41d991096ca08b
Deyan Diulgerov <chukstv@abv.bg>
2024-03-16 14:31:13 +0200

    Add files via upload

---
+++ LinkedListCycle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static bool LinkedListCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    return true;
+            }
+            return false;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
