33e9363589d4d082e5bb663ed1df48edc594e1c6
Deyan Diulgerov <chukstv@abv.bg>
2024-03-31 14:14:05 +0300

    Add files via upload

---
+++ HarshadNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarshadNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HarshadNumber(18));
+            Console.WriteLine(HarshadNumber(23));
+        }
+        public static int HarshadNumber(int x)
+        {
+            string strX = x.ToString();
+            int sum = 0;
+            for (int i = 0; i < strX.Length; i++)
+                sum += strX[i] - 48;
+            return x % sum == 0 ? sum : -1;
+        }
+    }
+}
