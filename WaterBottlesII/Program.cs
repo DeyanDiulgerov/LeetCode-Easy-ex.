76132d21807e76f4debe3521b6c722b265bcabb1
Deyan Diulgerov <chukstv@abv.bg>
2024-03-31 14:22:37 +0300

    Add files via upload

---
+++ WaterBottlesII/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterBottlesII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WaterBottlesII(13, 6));
+            Console.WriteLine(WaterBottlesII(10, 3));
+        }
+        public static int WaterBottlesII(int numBottles, int numExchange)
+        {
+            int emptyBottles = numBottles;
+            int resultBottles = numBottles;
+            numBottles = 0;
+            while (numBottles + emptyBottles >= numExchange)
+            {
+                while (emptyBottles >= numExchange)
+                {
+                    emptyBottles -= numExchange;
+                    numBottles++;
+                    numExchange++;
+                }
+                resultBottles += numBottles;
+                emptyBottles += numBottles;
+                numBottles = 0;
+            }
+            return resultBottles;
+        }
+    }
+}
