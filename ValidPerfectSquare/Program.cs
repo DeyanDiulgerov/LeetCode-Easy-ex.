6d720fbb432865851585ab9f3a32007a2e5fc667
Deyan Diulgerov <chukstv@abv.bg>
2024-04-02 15:26:19 +0300

    Add files via upload

---
+++ ValidPerfectSquare/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidPerfectSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidPerfectSquare(16));
+            Console.WriteLine(ValidPerfectSquare(14));
+            Console.WriteLine(ValidPerfectSquare(1));
+        }
+        public static bool ValidPerfectSquare(int num)
+        {
+            for (int i = 0; i <= num / 2 + 1; i++)
+            {
+                if (i * i == num)
+                    return true;
+                else if (i * i > num)
+                    return false;
+            }
+            return false;
+        }
+    }
+}
