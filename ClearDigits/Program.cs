99dd3c45914ecb0c945978c32beb74f5e6555716
Deyan Diulgerov <chukstv@abv.bg>
2024-07-18 10:32:30 +0300

    Add files via upload

---
+++ ClearDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClearDigits("abc"));
+            Console.WriteLine(ClearDigits("cb34"));
+        }
+        public static string ClearDigits(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char ch in s)
+            {
+                if (char.IsLetter(ch))
+                    stack.Push(ch);
+                else
+                    stack.Pop();
+            }
+            return String.Join("", stack.Reverse());
+        }
+    }
+}
