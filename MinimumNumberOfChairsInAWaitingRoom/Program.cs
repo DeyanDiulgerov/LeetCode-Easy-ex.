83f2d4c5804c4949944f68595d63f89492343cf6
Deyan Diulgerov <chukstv@abv.bg>
2024-07-18 10:07:52 +0300

    Add files via upload

---
+++ MinimumNumberOfChairsInAWaitingRoom/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumNumberOfChairsInAWaitingRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumNumberOfChairsInAWaitingRoom("EEEEEEE"));
+            Console.WriteLine(MinimumNumberOfChairsInAWaitingRoom("ELELEEL"));
+            Console.WriteLine(MinimumNumberOfChairsInAWaitingRoom("ELEELEELLL"));
+            Console.WriteLine(MinimumNumberOfChairsInAWaitingRoom("EL"));
+            Console.WriteLine(MinimumNumberOfChairsInAWaitingRoom("EEL"));
+        }
+        public static int MinimumNumberOfChairsInAWaitingRoom(string s)
+        {
+            int chairs = 0;
+            int res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'E')
+                    chairs++;
+                else
+                    chairs--;
+                res = Math.Max(chairs, res);
+            }
+            return res;
+        }
+    }
+}
