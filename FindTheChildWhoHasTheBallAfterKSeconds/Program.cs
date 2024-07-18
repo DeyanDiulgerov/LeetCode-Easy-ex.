cd6ba625e84208872fa0042a8ad55c06c7b2ec3e
Deyan Diulgerov <chukstv@abv.bg>
2024-07-18 10:46:48 +0300

    Add files via upload

---
+++ FindTheChildWhoHasTheBallAfterKSeconds/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheChildWhoHasTheBallAfterKSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheChildWhoHasTheBallAfterKSeconds(3, 5));
+            Console.WriteLine(FindTheChildWhoHasTheBallAfterKSeconds(5, 6));
+            Console.WriteLine(FindTheChildWhoHasTheBallAfterKSeconds(4, 2));
+        }
+        public static int FindTheChildWhoHasTheBallAfterKSeconds(int n, int k)
+        {
+            int i = 0;
+            while (k > 0)
+            {
+                if (i == n - 1)
+                {
+                    while (i > 0)
+                    {
+                        i--;
+                        k--;
+                        if (k == 0)
+                            break;
+                    }
+                }
+                else
+                {
+                    i++;
+                    k--;
+                    if (k == 0)
+                        break;
+                }
+            }
+            return i;
+        }
+    }
+}
