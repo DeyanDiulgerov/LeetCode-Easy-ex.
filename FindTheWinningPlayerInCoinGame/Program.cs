6639db5b24d8b1ebe4f4374c2a81101f86d55544
Deyan Diulgerov <chukstv@abv.bg>
2024-07-30 16:54:06 +0300

    Add files via upload

---
+++ FindTheWinningPlayerInCoinGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheWinningPlayerInCoinGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindTheWinningPlayerInCoinGame(2, 7));
+            Console.WriteLine(FindTheWinningPlayerInCoinGame(4, 11));
+        }
+        public static string FindTheWinningPlayerInCoinGame(int x, int y)
+        {
+            int turnCount = 0;
+            string res = "Bob";
+            while (x > 0 && y > 3)
+            {
+                x--;
+                y -= 4;
+                if (turnCount != 0)
+                {
+                    turnCount = 0;
+                    res = "Bob";
+                }
+                else
+                {
+                    turnCount = -1;
+                    res = "Alice";
+                }
+
+            }
+            return res;
+        }
+    }
+}
