fa29622074f539c249a36177a29f1bc51ae5e304
Deyan Diulgerov <chukstv@abv.bg>
2024-07-30 15:35:10 +0300

    Add files via upload

---
+++ FindifDigitGameCanBeWon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindifDigitGameCanBeWon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindifDigitGameCanBeWon(new int[] { 1, 2, 3, 4, 10 }));
+            Console.WriteLine(FindifDigitGameCanBeWon(new int[] { 1, 2, 3, 4, 5, 14 }));
+            Console.WriteLine(FindifDigitGameCanBeWon(new int[] { 5, 5, 5, 25 }));
+        }
+        public static bool FindifDigitGameCanBeWon(int[] nums)
+        {
+            var map = new Dictionary<string, int>();
+            map.Add("single", 0);
+            map.Add("double", 0);
+            foreach (int num in nums)
+            {
+                if (num >= 10)
+                    map["double"] += num;
+                else
+                    map["single"] += num;
+            }
+            return map["double"] != map["single"];
+        }
+    }
+}
