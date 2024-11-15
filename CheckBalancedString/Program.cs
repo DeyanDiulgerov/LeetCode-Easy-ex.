815c8b8326d61da31b3548f74c5d22f217cbabc0
Deyan Diulgerov <chukstv@abv.bg>
2024-11-15 17:11:14 +0200

    Add files via upload

---
+++ CheckBalancedString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckBalancedString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckBalancedString("1234"));
+            Console.WriteLine(CheckBalancedString("24123"));
+        }
+
+        public static bool CheckBalancedString(string num)
+        {
+            int oddSum = 0, evenSum = 0;
+            for (int i = 0; i < num.Length; i += 2)
+            {
+                evenSum += num[i] - 48;
+                if (i + 1 >= num.Length)
+                    break;
+                oddSum += num[i + 1] - 48;
+            }
+            return evenSum == oddSum;
+        }
+    }
+}
