988ea5f4b9ad6571825b9c934fd8cdd4979682ce
Deyan Diulgerov <chukstv@abv.bg>
2024-11-15 16:52:26 +0200

    Add files via upload

---
+++ SmallestDivisibleDigitProductI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestDivisibleDigitProductI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallestDivisibleDigitProductI(10, 2));
+            Console.WriteLine(SmallestDivisibleDigitProductI(15, 3));
+        }
+
+        public static int SmallestDivisibleDigitProductI(int n, int t)
+        {
+            for (int i = n; i < 1000; i++)
+            {
+                string str = i.ToString();
+                int product = 1;
+                foreach (char c in str)
+                    product *= c - 48;
+
+                if (product % t == 0)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
