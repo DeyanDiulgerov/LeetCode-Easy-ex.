1f1cea995da1c76a5766fd45bdf0868666f3019e
Deyan Diulgerov <chukstv@abv.bg>
2025-01-09 16:23:28 +0200

    Add files via upload

---
+++ CountSubarraysofLengthThreeWithaCondition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubarraysofLengthThreeWithaCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSubarraysofLengthThreeWithaCondition(new int[] { -1, -4, -1, 4 }));
+            Console.WriteLine(CountSubarraysofLengthThreeWithaCondition(new int[] { 1, 2, 1, 4, 1 }));
+            Console.WriteLine(CountSubarraysofLengthThreeWithaCondition(new int[] { 1, 1, 1 }));
+        }
+
+        public static int CountSubarraysofLengthThreeWithaCondition(int[] nums)
+        {
+            int res = 0;
+            int l = 0, r = 2;
+            while (r < nums.Count())
+            {
+                int sum = nums[l] + nums[r];
+                double divided = (double)nums[l + 1] / 2;
+                if (sum == divided)
+                    res++;
+                l++;
+                r++;
+            }
+            return res;
+        }
+    }
+}
